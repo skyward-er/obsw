fb6acdca670e44b8cb069cec8d15f3dea0af7324
Alvise De Faveri <alvise.defaveri@skywarder.eu>
2019-03-13 02:19:47 +0100

    [logproxy] Fixed missing log(), still some need fix from mavlink

--- src/boards/DeathStack/LogProxy/LogProxy.cpp
+++ src/boards/DeathStack/LogProxy/LogProxy.cpp
@@ -1,5 +1,5 @@
-/* Copyright (c) 2015-2018 Skyward Experimental Rocketry
- * Authors: Luca Erbetta
+/* Copyright (c) 2015-2019 Skyward Experimental Rocketry
+ * Authors: Alvise de' Faveri Tron, Alessio Galluccio
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -43,38 +43,72 @@ using namespace Status;
 namespace DeathStackBoard 
 {
 
+/**
+ * Each function here is an implementation of the log() method for a 
+ * specific status struct or class.
+ * The rationale is that, whenever a status struct is logged, the corresponding
+ * telemetry struct is updated synchronously in the telemetry repo.
+ * Telemetry requests from ground will therefore return the latest logged struct
+ * from each component.
+ */
+
+/* Flight Mode Manager */
 template <>
 LogResult LoggerProxy::log<FMMStatus>(const FMMStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO aggiornare pin_last_detection e pin_detection_count
     tm_repository.hm1_tm.state = static_cast<uint8_t>(t.state);
-
+    tm_repository.lr_tm.fmm_state = static_cast<uint8_t>(t.state);
 
     return logger.log(t);
 }
 
+/* Launch and Nosecone detachment pins */
+template <>
+LogResult LoggerProxy::log<PinStatus>(const PinStatus& t)
+{
+    miosix::PauseKernelLock kLock;
+
+    switch(t.pin)
+    {
+        /*
+        case ObservedPin::LAUNCH:
+            tm_repository.hm1_tm.launch_pin_last_detection = t.last_triggered_time;
+            tm_repository.hm1_tm.launch_pin_detection_count = t.num_triggered;
+            break;
+        // TODO Nosecone pin in telemetria
+        case ObservedPin::NOSECONE:
+            tm_repository.hm1_tm.nc_pin_last_detection = t.last_triggered_time;
+            tm_repository.hm1_tm.nc_pin_detection_count = t.num_triggered;
+            break;
+            */
+        default:
+            break;
+    }
+
+    return logger.log(t);
+}
+
+/* Ignition Board */
 template <>
 LogResult LoggerProxy::log<IgnBoardLoggableStatus>(const IgnBoardLoggableStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO sistemare il bitfield
-
     tm_repository.ign_tm.timestamp = t.timestamp;
-    //tm_repository.stm32_bitfield = t.board_status.avr_abortCmd;
-    //tm_repository.stm32_bitfield = t.board_status.avr_abortTimeout;
-    //tm_repository.stm32_bitfield = t.board_status.avr_abortWrongCode;
-    //tm_repository.stm32_bitfield = t.board_status.avr_launchDone;
-    //tm_repository.stm32_bitfield = t.board_status.stm32_powerOnReset;
-    //tm_repository.stm32_bitfield = t.board_status.stm32_externalReset;
-    //tm_repository.stm32_bitfield = t.board_status.stm32_brownoutReset;
-    //tm_repository.stm32_bitfield = t.board_status.stm32_watchdogReset;
+    uint8_t* bitfield_ptr = (uint8_t*)(&(t.board_status));
+    memcpy( &(tm_repository.ign_tm.avr_bitfield), 
+            &(bitfield_ptr[0]), 
+            sizeof(uint8_t) ); 
+    memcpy( &(tm_repository.ign_tm.stm32_bitfield), 
+            &(bitfield_ptr[1]), 
+            sizeof(uint8_t) );                  // Bitfield
    
     return logger.log(t);
 }
 
+/* Logger */
 template <>
 LogResult LoggerProxy::log<LogStats>(const LogStats& t)
 {
@@ -94,102 +128,108 @@ LogResult LoggerProxy::log<LogStats>(const LogStats& t)
     return logger.log(t);
 }
 
+/* TMTCManager (Mavlink) */
 template <>
 LogResult LoggerProxy::log<MavStatus>(const MavStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO sistemare mav_stats
-    // TODO controllare se è normale che ci siano solo questi elementi nella struct MavStatus
-    // TODO controllare tmtc_tm, alcuni nomi non corrispondono con la specifica Hermes Status
+    // mavchannel stats
     tm_repository.tmtc_tm.timestamp = t.timestamp;
     tm_repository.tmtc_tm.n_send_queue = t.n_send_queue;
     tm_repository.tmtc_tm.max_send_queue = t.max_send_queue;
     tm_repository.tmtc_tm.n_send_errors = t.n_send_errors;
-    //t.mav_stats;
+    tm_repository.tmtc_tm.msg_received = t.mav_stats.msg_received;   
+    // mav stats            
+    tm_repository.tmtc_tm.buffer_overrun = t.mav_stats.buffer_overrun;           
+    tm_repository.tmtc_tm.parse_error = t.mav_stats.parse_error;
+    tm_repository.tmtc_tm.parse_state = t.mav_stats.parse_state;
+    tm_repository.tmtc_tm.packet_idx = t.mav_stats.packet_idx;
+    tm_repository.tmtc_tm.current_rx_seq = t.mav_stats.current_rx_seq;           
+    tm_repository.tmtc_tm.current_tx_seq = t.mav_stats.current_tx_seq;           
+    tm_repository.tmtc_tm.packet_rx_success_count = t.mav_stats.packet_rx_success_count;
+    tm_repository.tmtc_tm.packet_rx_drop_count = t.mav_stats.packet_rx_drop_count;  
    
     return logger.log(t);
 }
 
+/* Sensor Manager */
 template <>
 LogResult LoggerProxy::log<SensorManagerStatus>(const SensorManagerStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO aggiornare la status repo
+    tm_repository.sm_tm.timestamp = t.timestamp;
+    tm_repository.sm_tm.sensor_init_state = t.problematic_sensors;
+    tm_repository.sm_tm.state = (uint8_t) t.state;
    
     return logger.log(t);
 }
 
+/* Ignition Controller */
 template <>
 LogResult LoggerProxy::log<IgnCtrlStatus>(const IgnCtrlStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-<<<<<<< HEAD
-    // TODO in ign_ctrl_tm, è da aggiungere una s finale a n_rcv_message per essere conforme con la specifica
-    // TODO manca cmd_bitfield, dove bisogna mettere abort_rcv, abort_sent, launch_sent e padding
-    // TODO inserire nel commento di cmd_bitfield di mavlink_ign_ctrl_tm_t anche la presenza di padding
-
     tm_repository.ign_ctrl_tm.timestamp = t.timestamp;
     tm_repository.ign_ctrl_tm.fsm_state = t.fsm_state;
     tm_repository.ign_ctrl_tm.last_event = t.last_event;
+    // TODO add s
     tm_repository.ign_ctrl_tm.n_rcv_message = t.n_rcv_messages;
     tm_repository.ign_ctrl_tm.n_sent_messages = t.n_sent_messages;
-    //tm_repository.ign_ctrl_tm.cmd_bitfield = t.launch_sent;
-    //tm_repository.ign_ctrl_tm.cmd_bitfield = t.abort_rcv;
-    //tm_repository.ign_ctrl_tm.cmd_bitfield = t.abort_sent;
-    //tm_repository.ign_ctrl_tm.cmd_bitfield = t.padding;
-=======
-    tm_repository.ign_ctrl_tm.timestamp = t.timestamp;
-    tm_repository.ign_ctrl_tm.fsm_state = t.fsm_state;
-    tm_repository.ign_ctrl_tm.last_event = t.last_event;
-    tm_repository.ign_ctrl_tm.n_rcv_message= t.n_rcv_messages;
-    tm_repository.ign_ctrl_tm.n_sent_messages = t.n_sent_messages;
-    tm_repository.ign_ctrl_tm.last_event = t.last_event;
 
     // Bitfield
     tm_repository.ign_ctrl_tm.cmd_bitfield = 0;
     tm_repository.ign_ctrl_tm.cmd_bitfield &= t.launch_sent;
     tm_repository.ign_ctrl_tm.cmd_bitfield &= (t.abort_sent << 1);
     tm_repository.ign_ctrl_tm.cmd_bitfield &= (t.abort_rcv << 2);
->>>>>>> 46f536c812aa27173ba5376758b1a7076457f465
    
     return logger.log(t);
 }
 
+/* Deployment Controller */
 template <>
 LogResult LoggerProxy::log<DeploymentStatus>(const DeploymentStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO sistemare il cast dei tipi motor_last_direction e cutter_state
     tm_repository.dpl_ctrl_tm.timestamp = t.timestamp;
-    tm_repository.dpl_ctrl_tm.fsm_state = t.state;
+    tm_repository.dpl_ctrl_tm.fsm_state = (uint8_t) t.state;
     tm_repository.dpl_ctrl_tm.motor_active = t.motor_status.motor_active;
-    tm_repository.dpl_ctrl_tm.motor_last_direction = t.motor_status.motor_last_direction;
-    tm_repository.dpl_ctrl_tm.cutter_state = t.cutter_status.state;
+    tm_repository.dpl_ctrl_tm.motor_last_direction = (uint8_t) t.motor_status.motor_last_direction;
+    tm_repository.dpl_ctrl_tm.cutter_state = (uint8_t) t.cutter_status.state;
    
     return logger.log(t);
 }
 
+/* ADA state machine */
 template <>
 LogResult LoggerProxy::log<ADAStatus>(const ADAStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO sistema cast dei tipi stae e last_apogee_state
-    // TODO target_dpl_pressure non è presente. Controllare se è da aggiungere
-    // TO DO last_dpl_pressure_tick ha un nome diverso rispetto alle speciiche
     tm_repository.ada_tm.timestamp = t.timestamp;
-    tm_repository.ada_tm.state = t.state;
-    tm_repository.ada_tm.last_apogee_state = t.last_apogee.state;
+    tm_repository.ada_tm.state = (uint8_t) t.state;
+    tm_repository.ada_tm.last_apogee_state = (uint8_t) t.last_apogee.state;
     tm_repository.ada_tm.last_apogee_tick = t.last_apogee.tick;
     tm_repository.ada_tm.last_dpl_pressure_tick = t.last_dpl_pressure_tick;
    
     return logger.log(t);
 }
 
+/* ADA target dpl pressure */
+template <>
+LogResult LoggerProxy::log<TargetDeploymentPressure>(const TargetDeploymentPressure& t)
+{
+    miosix::PauseKernelLock kLock;
+
+    tm_repository.ada_tm.target_dpl_pressure = t.deployment_pressure;
+   
+    return logger.log(t);
+}
+
+/* ADA kalman filter values */
 template <>
 LogResult LoggerProxy::log<KalmanState>(const KalmanState& t)
 {
@@ -202,12 +242,12 @@ LogResult LoggerProxy::log<KalmanState>(const KalmanState& t)
     return logger.log(t);
 }
 
+/* Canbus stats */
 template <>
 LogResult LoggerProxy::log<CanStatus>(const CanStatus& t)
 {
     miosix::PauseKernelLock kLock;
 
-    // TODO nomi non corrispondono a specifica
     tm_repository.can_tm.n_sent = t.n_sent;
     tm_repository.can_tm.n_rcv = t.n_rcv;
     tm_repository.can_tm.last_sent = t.last_sent;
@@ -218,24 +258,28 @@ LogResult LoggerProxy::log<CanStatus>(const CanStatus& t)
     return logger.log(t);
 }
 
+/* Main Barometer */
 template <>
 LogResult LoggerProxy::log<AD7994WrapperData>(const AD7994WrapperData& t)
 {
     miosix::PauseKernelLock kLock;
 
-    //nomi non corrispondono a specifiche
     // TODO manca val_ch1_min
-    // TO DO la classe AD7994WrapperData ha dei valori in più, come i flag
     tm_repository.ad7994_tm.timestamp = t.timestamp;
     tm_repository.ad7994_tm.val_ch1 = t.ch1_pressure;
     //t.val_ch1_min
-    tm_repository.ad7994_tm.val_ch2 = t.ch2_pressure;
     tm_repository.ad7994_tm.mean_ch1 = t.ch1_mean;
     tm_repository.ad7994_tm.stddev_ch1 = t.ch1_stddev;
+    tm_repository.ad7994_tm.val_ch2 = t.ch2_pressure;
+
+    // LR_TM
+    tm_repository.lr_tm.baro1_val = t.ch1_pressure;
+    //tm_repository.lr_tm.baro1_min = t.val_ch1_min;
    
     return logger.log(t);
 }
 
+/* Battery status, sampled by internal ADC */
 template <>
 LogResult LoggerProxy::log<BatteryTensionData>(const BatteryTensionData& t)
 {
@@ -244,33 +288,32 @@ LogResult LoggerProxy::log<BatteryTensionData>(const BatteryTensionData& t)
     tm_repository.adc_tm.timestamp = t.timestamp;
     tm_repository.adc_tm.battery_tension = t.battery_tension_value;
     tm_repository.adc_tm.battery_tension_min = t.battery_tension_min;
+    // LR_TM
+    tm_repository.lr_tm.battery_tension_min = t.battery_tension_min;
    
     return logger.log(t);
 }
 
+/* Motor current sense, sampled by internal ADC */
 template <>
 LogResult LoggerProxy::log<CurrentSenseData>(const CurrentSenseData& t)
 {
     miosix::PauseKernelLock kLock;
 
-<<<<<<< HEAD
-    //TO DO anche questo aggiorna timestamp, oltre a BatteryTensionData. Controllare.
     tm_repository.adc_tm.timestamp = t.timestamp;
     tm_repository.adc_tm.current_sense_1 = t.current_1_value;
     tm_repository.adc_tm.current_sense_2 = t.current_2_value;
-=======
-    // TODO aggiornare la status repo
 
     return logger.log(t);
 }
 
+/* ADIS imu */
 template <>
 LogResult LoggerProxy::log<ADIS16405Data>(const ADIS16405Data& t)
 {
     miosix::PauseKernelLock kLock;
 
     tm_repository.adis_tm.timestamp = miosix::getTick();
-
     tm_repository.adis_tm.acc_x = t.xaccl_out;
     tm_repository.adis_tm.acc_y = t.yaccl_out;
     tm_repository.adis_tm.acc_z = t.zaccl_out;
@@ -280,22 +323,20 @@ LogResult LoggerProxy::log<ADIS16405Data>(const ADIS16405Data& t)
     tm_repository.adis_tm.compass_x = t.xmagn_out;
     tm_repository.adis_tm.compass_y = t.ymagn_out;
     tm_repository.adis_tm.compass_z = t.zmagn_out;
-
     tm_repository.adis_tm.temp = t.temp_out;
     tm_repository.adis_tm.supply_out = t.supply_out;
     tm_repository.adis_tm.aux_adc = t.aux_adc;
->>>>>>> 46f536c812aa27173ba5376758b1a7076457f465
    
     return logger.log(t);
 }
 
+/* MPU imu */
 template <>
 LogResult LoggerProxy::log<MPU9250Data>(const MPU9250Data& t)
 {
     miosix::PauseKernelLock kLock;
 
     tm_repository.mpu_tm.timestamp = miosix::getTick();
-
     tm_repository.mpu_tm.acc_x = t.accel.getX();
     tm_repository.mpu_tm.acc_y = t.accel.getY();
     tm_repository.mpu_tm.acc_z = t.accel.getZ();
@@ -305,12 +346,12 @@ LogResult LoggerProxy::log<MPU9250Data>(const MPU9250Data& t)
     tm_repository.mpu_tm.compass_x = t.compass.getX();
     tm_repository.mpu_tm.compass_y = t.compass.getY();
     tm_repository.mpu_tm.compass_z = t.compass.getZ();
-
     tm_repository.mpu_tm.temp = t.temp;
    
     return logger.log(t);
 }
 
+/* GPS */
 template <>
 LogResult LoggerProxy::log<GPSData>(const GPSData& t)
 {
@@ -325,7 +366,6 @@ LogResult LoggerProxy::log<GPSData>(const GPSData& t)
     tm_repository.gps_tm.vel_east = t.velocityEast;
     tm_repository.gps_tm.vel_down = t.velocityDown;
     tm_repository.gps_tm.vel_mag = t.speed;
-
     tm_repository.gps_tm.fix = (uint8_t) t.fix;
     tm_repository.gps_tm.n_satellites = t.numSatellites;
 
@@ -340,14 +380,14 @@ LogResult LoggerProxy::log<GPSData>(const GPSData& t)
     return logger.log(t);
 }
 
-// Sorry but it's much clearer without repeated code
+/* Sensor Manager Tasks */
+// Sorry for function-like macro, too lazy to copy-paste
 #define UPDATE_TASK(n) \
             tm_repository.sm_task##n##_tm.task_##n##_id = t.id; \
             tm_repository.sm_task##n##_tm.task_##n##_min_value = t.activationStats.minValue; \
             tm_repository.sm_task##n##_tm.task_##n##_max_value = t.activationStats.maxValue; \
             tm_repository.sm_task##n##_tm.task_##n##_mean_value = t.activationStats.mean; \
             tm_repository.sm_task##n##_tm.task_##n##_stddev = t.activationStats.stdev;
-
 template <>
 LogResult LoggerProxy::log<TaskStatResult>(const TaskStatResult& t)
 {
