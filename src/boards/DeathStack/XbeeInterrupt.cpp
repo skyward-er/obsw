7cfa6b9d620d62ee2926facaedc6b2f5f3794014
Luca Erbetta <luca.erbetta@skywarder.eu>
2019-05-08 18:08:35 +0200

    Added xbee-interrupt files

---
+++ src/boards/DeathStack/XbeeInterrupt.cpp
@@ -0,0 +1,38 @@
+#include "XbeeInterrupt.h"
+#include <miosix.h>
+#include <drivers/Xbee/Xbee.h>
+
+using namespace miosix;
+
+void __attribute__((used)) EXTI10_IRQHandlerImpl() { Xbee::handleInterrupt(); }
+
+namespace DeathStackBoard
+{
+void enableXbeeInterrupt()
+{
+    {
+        FastInterruptDisableLock l;
+        RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    }
+    // Refer to the datasheet for a detailed description on the procedure and
+    // interrupt registers
+
+    // Clear the mask on the wanted line
+    EXTI->IMR |= EXTI_IMR_MR10;
+
+    // Trigger the interrupt on a falling edge
+    EXTI->FTSR |= EXTI_FTSR_TR10;
+
+    // Trigger the interrupt on a rising edge
+    // EXTI->RTSR |= EXTI_RTSR_TR0;
+
+    EXTI->PR |= EXTI_PR_PR10;  // Reset pending register
+
+    // Enable interrupt on PF10 in SYSCFG
+    SYSCFG->EXTICR[2] = 0x5U << 8;
+
+    // Enable the interrput in the interrupt controller
+    NVIC_EnableIRQ(EXTI15_10_IRQn);
+    NVIC_SetPriority(EXTI15_10_IRQn, 15);
+}
+}
