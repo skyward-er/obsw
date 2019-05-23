e9ae68af6f7214d64c91e65d57a7bd8b14bcf78a
Luca Erbetta <luca.erbetta@skywarder.eu>
2019-05-23 13:57:32 +0200

    Fix copyright notice

--- src/tests/catch/ada/test-ada-data.h
+++ src/tests/catch/ada/test-ada-data.h
@@ -1,25 +1,26 @@
 /*
  * Copyright (c) 2019 Skyward Experimental Rocketry
  * Authors: Luca Mozzarelli
- *
- * Permission is hereby grantedf, free of chargef, to any person obtaining a copy
- * of this software and associated documentation files (the "Software")f, to deal
- * in the Software without restrictionf, including without limitation the rights
- * to usef, copyf, modifyf, mergef, publishf, distributef, sublicensef, and/or sell
- * copies of the Softwaref, and to permit persons to whom the Software is
- * furnished to do sof, subject to the following conditions:
- *
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS"f, WITHOUT WARRANTY OF ANY KINDf, EXPRESS OR
- * IMPLIEDf, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIMf, DAMAGES OR OTHER
- * LIABILITYf, WHETHER IN AN ACTION OF CONTRACTf, TORT OR OTHERWISEf, ARISING FROM,
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 #pragma once
 
 static const unsigned DATA_SIZE = 4840;
