b52b386be34d1869c88ab008e2be11808cc40608
Luca Conterio <luca.conterio@skywarder.eu>
2021-05-23 19:15:42 +0200

    [BMX160Calibrator] Added sketch for ramp calibration
    - renamed BMX160Corrector to BMX160calibrator
    - separated BMX160DataCorrected in a separate file

---
+++ src/boards/DeathStack/Sensors/BMX160DataCorrected.h
@@ -0,0 +1,41 @@
+/* Copyright (c) 2021 Skyward Experimental Rocketry
+ * Authors: Luca Conterio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "sensors/BMX160/BMX160Data.h"
+
+struct BMX160DataCorrected : public BMX160Data
+{
+    BMX160DataCorrected() : BMX160Data() {}
+
+    BMX160DataCorrected(const BMX160Data& data)
+        : BMX160DataCorrected(data, data, data)
+    {
+    }
+
+    BMX160DataCorrected(AccelerometerData acc, GyroscopeData gyr,
+                        MagnetometerData mag)
+        : BMX160Data(acc, gyr, mag)
+    {
+    }
+};
