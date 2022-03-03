ee2de60e4d302aeac2f9dfc27e54be30bacc09b8
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-03-03 12:09:52 +0100

    Moved Lynx test entrypoints in side directory

---
+++ src/tests Lynx/catch/ada/kalman_acc/test-kalman-acc-data.h
@@ -0,0 +1,33 @@
+/* Copyright (c) 2019 Skyward Experimental Rocketry
+ * Author: Luca Mozzarelli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+static const unsigned DATA_SIZE_P = 700;
+extern const float SIMULATED_PRESSURE[DATA_SIZE_P];
+extern const float SIMULATED_Z[DATA_SIZE_P];
+extern const float SIMULATED_VZ[DATA_SIZE_P];
+
+static const unsigned DATA_SIZE_AX = 17495;
+extern const float SIMULATED_AX[DATA_SIZE_AX];
+
+// extern const float SIMULATED_NOISY_PRESSURE[DATA_SIZE];
---
+++ src/tests Lynx/test-sm+tmtc.cpp
@@ -0,0 +1,115 @@
+
+/* Copyright (c) 2018-2019 Skyward Experimental Rocketry
+ * Author: Alvise de'Faveri Tron
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <TMTCManager/TMTCManager.h>
+#include <events/Events.h>
+#include <events/Topics.h>
+#include <interfaces-impl/hwmapping.h>
+
+#include "ADA/ADAController.h"
+#include "LoggerService/LoggerService.h"
+#include "SensorManager/SensorManager.h"
+#include "diagnostic/CpuMeter.h"
+#include "events/EventBroker.h"
+
+using namespace miosix;
+using namespace DeathStackBoard;
+
+int main()
+{
+    printf("** Sensor manager & TMTC test **\n");
+    i2c1::init();
+    busSPI2::init();
+    /******************
+     * SENSOR MANAGER *
+     ******************/
+    Stats s;
+    ADAController ada;
+    SensorManager mgr{&ada};
+    // ada.start();
+    // try
+    // {
+    //     LoggerService::getInstance().start();
+    // }
+    // catch (const std::exception& e)
+    // {
+    //     printf("SDCARD MISSING\n");;
+    //     for (;;)
+    //     {
+    //         led1::high();
+    //         Thread::sleep(200);
+    //         led1::low();
+    //         Thread::sleep(200);
+    //     }
+    // }
+    // led1::high();
+
+    sEventBroker.start();
+    mgr.start();
+
+    sEventBroker.post({EV_TC_START_SENSOR_LOGGING}, TOPIC_TC);
+
+    printf("Wait for calibration to complete.\n");
+
+    Thread::sleep(500);
+
+    /******************
+     *  TMTC MANAGER  *
+     ******************/
+
+    TMTCManager* tmtc = new TMTCManager();
+    tmtc->start();
+    sEventBroker.start();
+
+    Thread::sleep(1000);
+
+    // printf("\nOk, press open to post liftoff...\n");
+    // while(inputs::btn_open::value())
+    // {
+    //     Thread::sleep(100);
+    // }
+
+    printf("Spamming HR_TM\n");
+
+    Thread::sleep(100);
+
+    sEventBroker.post({EV_LIFTOFF}, TOPIC_FLIGHT_EVENTS);
+
+    for (;;)
+    {
+        led1::high();
+        Thread::sleep(1000);
+        led1::low();
+        Thread::sleep(1000);
+    }
+
+    printf("Press close to reboot...\n");
+    while (inputs::btn_close::value())
+    {
+        Thread::sleep(100);
+    }
+
+    miosix::reboot();
+
+    return 0;
+}
