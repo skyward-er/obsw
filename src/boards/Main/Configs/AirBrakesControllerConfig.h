96c134ae6a55617b16521e7757c7716d2c8cf55c
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-10-11 18:40:05 +0100

    [AirBrakes] Updated shadow mode timeout

--- src/boards/Main/Configs/AirBrakesControllerConfig.h
+++ src/boards/Main/Configs/AirBrakesControllerConfig.h
@@ -34,7 +34,7 @@ namespace AirBrakesControllerConfig
 constexpr uint32_t UPDATE_PERIOD = 100;  // 10 hz
 
 #ifdef EUROC
-static constexpr int SHADOW_MODE_TIMEOUT = 5.4 * 1000;
+static constexpr int SHADOW_MODE_TIMEOUT = 5.7 * 1000;
 #else
 static constexpr int SHADOW_MODE_TIMEOUT = 3.8 * 1000;
 #endif
