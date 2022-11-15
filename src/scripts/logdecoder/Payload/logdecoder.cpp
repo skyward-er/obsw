a29c08ea0426ef1746d4dc45680391ae0ee90c82
Federico Mandelli <federico.mandelli@skywarder.eu>
2022-11-15 14:19:31 +0100

    [Copyright] Fixed copyright naming in some files

--- src/scripts/logdecoder/Payload/logdecoder.cpp
+++ src/scripts/logdecoder/Payload/logdecoder.cpp
@@ -1,5 +1,5 @@
 /* Copyright (c) 2018-2022 Skyward Experimental Rocketry
- * Author: Terrane Federico
+ * Author: Federico Terraneo
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
