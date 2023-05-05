b38f50a30b4129841c8d517dc55d92ec0c0f819a
Riccardo Musso <riccardo.musso@skywarder.eu>
2023-05-05 22:45:34 +0200

    [Converter] implemented simple convertion function from NED to angles

---
+++ src/entrypoints/Groundstation/automated-antennas-entry.cpp
@@ -0,0 +1,30 @@
+#include <drivers/timer/TimestampTimer.h>
+#include <miosix.h>
+
+#include "AutomatedAntennas/Converter.h"
+
+using namespace miosix;
+using namespace Boardcore;
+
+inline float randf() { return (std::rand() % 200 - 100) / 100.f; }
+
+int main()
+{
+    constexpr int N = 10000;
+
+    printf("Starting test\n");
+    uint64_t start = TimestampTimer::getTimestamp();
+
+    for (int i = 0; i < N; i++)
+    {
+        NEDCoords coords     = {randf(), randf(), randf()};
+        AntennaAngles angles = rocketPositionToAntennaAngles(coords);
+        printf("NED: %.2f ; %.2f ; %.2f -> Angles %.2f ; %.2f\n", coords.n,
+               coords.e, coords.d, angles.theta1, angles.theta2);
+    }
+
+    uint64_t end = TimestampTimer::getTimestamp();
+    printf("Took %lld millis for %d calls.\n", (end - start) / 1000ull, N);
+
+    return 0;
+}
