777a26819f70809a37e1c27603f10ea9fb23df86
Davide Mor <tazdevil971@gmail.com>
2023-07-26 23:23:54 +0200

    [gs] Added shared radio configurations

---
+++ src/boards/common/Radio.h
@@ -0,0 +1,52 @@
+/* Copyright (c) 2023 Skyward Experimental Rocketry
+ * Author: Davide Mor
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <radio/SX1278/SX1278Fsk.h>
+
+namespace Common
+{
+
+static const Boardcore::SX1278Fsk::Config MAIN_RADIO_CONFIG = {
+    .freq_rf    = 434000000,
+    .freq_dev   = 50000,
+    .bitrate    = 48000,
+    .rx_bw      = Boardcore::SX1278Fsk::Config::RxBw::HZ_125000,
+    .afc_bw     = Boardcore::SX1278Fsk::Config::RxBw::HZ_125000,
+    .ocp        = 120,
+    .power      = 13,
+    .shaping    = Boardcore::SX1278Fsk::Config::Shaping::GAUSSIAN_BT_1_0,
+    .dc_free    = Boardcore::SX1278Fsk::Config::DcFree::WHITENING,
+    .enable_crc = false};
+
+static const Boardcore::SX1278Fsk::Config PAYLOAD_RADIO_CONFIG = {
+    .freq_rf    = 868000000,
+    .freq_dev   = 50000,
+    .bitrate    = 48000,
+    .rx_bw      = Boardcore::SX1278Fsk::Config::RxBw::HZ_125000,
+    .afc_bw     = Boardcore::SX1278Fsk::Config::RxBw::HZ_125000,
+    .ocp        = 120,
+    .power      = 13,
+    .shaping    = Boardcore::SX1278Fsk::Config::Shaping::GAUSSIAN_BT_1_0,
+    .dc_free    = Boardcore::SX1278Fsk::Config::DcFree::WHITENING,
+    .enable_crc = false};
+
+}  // namespace Common
