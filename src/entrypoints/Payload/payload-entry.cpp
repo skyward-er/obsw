e8166e8de32b181fb4d7e007bc730cd5aa096e55
Federico Mandelli <federicomandelli@skywarder.eu>
2023-08-01 09:00:25 +0200

    [Test] Created first entrypoint

---
+++ src/entrypoints/Payload/payload-entry.cpp
@@ -0,0 +1,175 @@
+/* Copyright (c) 2023 Skyward Experimental Rocketry
+ * Author: Matteo Pignataro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <Payload/BoardScheduler.h>
+#include <Payload/Buses.h>
+#include <Payload/Sensors/Sensors.h>
+#include <common/Events.h>
+#include <common/Topics.h>
+#include <diagnostic/CpuMeter/CpuMeter.h>
+#include <diagnostic/PrintLogger.h>
+#include <diagnostic/StackLogger.h>
+#include <drivers/timer/TimestampTimer.h>
+#include <events/EventBroker.h>
+#include <events/EventData.h>
+#include <events/utils/EventSniffer.h>
+
+#include <utils/ModuleManager/ModuleManager.hpp>
+
+using namespace Boardcore;
+using namespace Payload;
+using namespace Common;
+
+int main()
+{
+    ModuleManager& modules = ModuleManager::getInstance();
+
+    // Overall status, if at some point it becomes false, there is a problem
+    // somewhere
+    bool initResult    = true;
+    PrintLogger logger = Logging::getLogger("Payload");
+
+    // Create modules
+    BoardScheduler* scheduler = new BoardScheduler();
+    Buses* buses              = new Buses();
+    Sensors* sensors =
+        new Sensors(scheduler->getScheduler(miosix::PRIORITY_MAX - 1));
+    // NASController* nas =
+    //     new NASController(scheduler->getScheduler(miosix::PRIORITY_MAX));
+    // Radio* radio = new Radio(scheduler->getScheduler(miosix::PRIORITY_MAX -
+    // 2)); TMRepository* tmRepo = new TMRepository(); CanHandler* canHandler =
+    //     new CanHandler(scheduler->getScheduler(miosix::PRIORITY_MAX - 2));
+
+    // Insert modules
+    if (!modules.insert<BoardScheduler>(scheduler))
+    {
+        initResult = false;
+        LOG_ERR(logger, "Error inserting the board scheduler module");
+    }
+
+    if (!modules.insert<Buses>(buses))
+    {
+        initResult = false;
+        LOG_ERR(logger, "Error inserting the buses module");
+    }
+
+    if (!modules.insert<Sensors>(sensors))
+    {
+        initResult = false;
+        LOG_ERR(logger, "Error inserting the sensor module");
+    }
+
+    // if (!modules.insert<NASController>(nas))
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error inserting the NAS module");
+    // }
+
+    // if (!modules.insert<Radio>(radio))
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error inserting the Radio module");
+    // }
+
+    // if (!modules.insert<TMRepository>(tmRepo))
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error inserting the TMRepository module");
+    // }
+
+    // if (!modules.insert<CanHandler>(canHandler))
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error inserting the CanHandler module");
+    // }
+
+    // Start modules
+    if (!EventBroker::getInstance().start())
+    {
+        initResult = false;
+        LOG_ERR(logger, "Error starting the EventBroker module");
+    }
+
+    if (!modules.get<BoardScheduler>()->start())
+    {
+        initResult = false;
+        LOG_ERR(logger, "Error starting the board scheduler module");
+    }
+
+    if (!modules.get<Sensors>()->start())
+    {
+        initResult = false;
+        LOG_ERR(logger, "Error starting the sensors module");
+    }
+
+    // if (!modules.get<NASController>()->start())
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error starting the NAS module");
+    // }
+
+    // if (!modules.get<Radio>()->start())
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error starting the Radio module");
+    // }
+
+    // if (!modules.get<CanHandler>()->start())
+    // {
+    //     initResult = false;
+    //     LOG_ERR(logger, "Error starting the CanHandler module");
+    // }
+
+    // Log all the events
+    EventSniffer sniffer(
+        EventBroker::getInstance(), TOPICS_LIST,
+        [](uint8_t event, uint8_t topic)
+        {
+            EventData ev{TimestampTimer::getTimestamp(), event, topic};
+            Logger::getInstance().log(ev);
+        });
+
+    // Check the init result and launch an event
+    if (initResult)
+    {
+        // Post OK
+        EventBroker::getInstance().post(FMM_INIT_OK, TOPIC_FMM);
+
+        // Set the LED status
+        miosix::led1On();
+    }
+    else
+    {
+        EventBroker::getInstance().post(FMM_INIT_ERROR, TOPIC_FMM);
+    }
+
+    // Periodic statistics
+    while (true)
+    {
+        Thread::sleep(1000);
+        Logger::getInstance().log(CpuMeter::getCpuStats());
+        CpuMeter::resetCpuStats();
+        StackLogger::getInstance().log();
+    }
+
+    return 0;
+}
