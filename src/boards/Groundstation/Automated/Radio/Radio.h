301c584b9d8a1e5f6bc4eb2213077c9db7df675c
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2023-08-11 01:20:44 +0200

    [AutomatedAntennas] Ported the Radio module from the base Groundstation

--- src/boards/Groundstation/Automated/Radio/Radio.h
+++ src/boards/Groundstation/Automated/Radio/Radio.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2023 Skyward Experimental Rocketry
- * Authors: Riccardo Musso, Emilio Corigliano
+ * Author: Davide Mor
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,38 +20,17 @@
  * THE SOFTWARE.
  */
 
-#include <drivers/timer/TimestampTimer.h>
-#include <miosix.h>
+#pragma once
 
-#include <Groundstation/Base/Buses.h>
-#include <Groundstation/Base/Hub.h>
-#include <Groundstation/Base/Radio/Radio.h>
-#include <Groundstation/Base/Radio/RadioStatus.h>
-#include <Groundstation/Common/Ports/Serial.h>
-#include "Converter.h"
+#include <Groundstation/Common/Radio/RadioBase.h>
 
-using namespace miosix;
-using namespace Boardcore;
-
-inline float randf() { return (std::rand() % 200 - 100) / 100.f; }
-
-int main()
+namespace Antennas
 {
-    constexpr int N = 10000;
 
-    printf("Starting test\n");
-    uint64_t start = TimestampTimer::getTimestamp();
+class RadioMain : public Groundstation::RadioBase, public Boardcore::Module
+{
+public:
+    [[nodiscard]] bool start();
+};
 
-    for (int i = 0; i < N; i++)
-    {
-        NEDCoords coords     = {randf(), randf(), randf()};
-        AntennaAngles angles = rocketPositionToAntennaAngles(coords);
-        printf("NED: %.2f ; %.2f ; %.2f -> Angles %.2f ; %.2f\n", coords.n,
-               coords.e, coords.d, angles.theta1, angles.theta2);
-    }
-
-    uint64_t end = TimestampTimer::getTimestamp();
-    printf("Took %llu millis for %d calls.\n", (end - start) / 1000ull, N);
-
-    return 0;
-}
+}  // namespace Antennas
