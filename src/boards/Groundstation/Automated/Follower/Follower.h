59ff11fe3d1b3e40a33316b54a9cceb2d5b8099f
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2023-08-20 01:36:33 +0200

    [AutomatedAntennas] Implemented first version of the Follower algorithm

---
+++ src/boards/Groundstation/Automated/Follower/Follower.h
@@ -0,0 +1,83 @@
+/* Copyright (c) 2023 Skyward Experimental Rocketry
+ * Author: Emilio Corigliano
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <Groundstation/Automated/Config/FollowerConfig.h>
+#include <algorithms/Algorithm.h>
+#include <algorithms/NAS/NASState.h>
+#include <diagnostic/PrintLogger.h>
+#include <logger/Logger.h>
+#include <sensors/SensorData.h>
+
+#include <utils/ModuleManager/ModuleManager.hpp>
+
+namespace Antennas
+{
+
+struct NEDCoords
+{
+    float n = 0;
+    float e = 0;
+    float d = 0;
+};
+
+struct AntennaAngles
+{
+    float theta1 = 0;
+    float theta2 = 0;
+};
+
+class Follower : public Boardcore::Algorithm, public Boardcore::Module
+{
+public:
+    Follower();
+
+    bool init() override;
+
+    /**
+     * @param get rocket NAS state.
+     */
+    Boardcore::NASState getLastRocketNasState();
+    Boardcore::GPSData getLastRocketGpsState();
+
+    /**
+     * @param set rocket NAS state.
+     */
+    void setLastRocketState(const Boardcore::NASState& nas,
+                            const Boardcore::GPSData& gps);
+
+private:
+    void step() override;
+
+    AntennaAngles rocketPositionToAntennaAngles(const NEDCoords& ned);
+
+    const uint8_t maxInitRetries =
+        10;  ///< max number of retries for GPS data acquisition
+    Boardcore::NASState lastRocketNasState;
+    Boardcore::GPSData lastRocketGpsState;
+    Boardcore::GPSData antennaPosition;
+
+    Boardcore::PrintLogger logger = Boardcore::Logging::getLogger("Follower");
+};
+
+}  // namespace Antennas
