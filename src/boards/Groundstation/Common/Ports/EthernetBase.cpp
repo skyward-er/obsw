a7d8c3964e9a3836583b257ff41bb243751b381f
Davide Mor <davide.mor@skywarder.eu>
2023-08-29 18:16:36 +0200

    [gs] Added ethernet port

--- src/boards/Groundstation/Common/Ports/EthernetBase.cpp
+++ src/boards/Groundstation/Common/Ports/EthernetBase.cpp
@@ -20,37 +20,35 @@
  * THE SOFTWARE.
  */
 
-#include "EthernetBase.h"
-
-#include <Groundstation/Common/Config/EthernetConfig.h>
-#include <Groundstation/Common/HubBase.h>
-
 #include <random>
+
+#include <Groundstation/Common/HubBase.h>
+#include <Groundstation/Common/Config/EthernetConfig.h>
+
+#include "EthernetBase.h"
 
 using namespace Groundstation;
 using namespace Boardcore;
 using namespace miosix;
 
-WizIp Groundstation::genNewRandomIp()
-{
+WizIp Groundstation::genNewRandomIp() {
     WizIp ip = IP_BASE;
-    ip.d     = (rand() % 253) + 1;  // Generate in range 1-254
+    ip.d = (rand() % 253) + 1; // Generate in range 1-254
 
     return ip;
 }
 
-WizMac Groundstation::genNewRandomMac()
-{
+WizMac Groundstation::genNewRandomMac() {
     WizMac mac = MAC_BASE;
-    mac.e      = (rand() % 253) + 1;  // Generate in range 1-254
-    mac.f      = (rand() % 253) + 1;  // Generate in range 1-254
+    mac.e = (rand() % 253) + 1; // Generate in range 1-254
+    mac.f = (rand() % 253) + 1; // Generate in range 1-254
 
     return mac;
 }
 
 void EthernetBase::handleINTn()
 {
-    if (wiz5500)
+    if (started)
     {
         wiz5500->handleINTn();
     }
@@ -58,15 +56,12 @@ void EthernetBase::handleINTn()
 
 void EthernetBase::sendMsg(const mavlink_message_t& msg)
 {
-    if (mav_driver && mav_driver->isStarted())
-    {
-        mav_driver->enqueueMsg(msg);
+    if(started) {
+        uint8_t msg_buf[MAVLINK_NUM_NON_PAYLOAD_BYTES +
+                        MAVLINK_MAX_DIALECT_PAYLOAD_SIZE];
+        int msg_len = mavlink_msg_to_send_buffer(msg_buf, &msg);
+        wiz5500->send(0, msg_buf, msg_len, 100);
     }
-}
-
-Boardcore::Wiz5500::PhyState EthernetBase::getState()
-{
-    return wiz5500->getPhyState();
 }
 
 bool EthernetBase::start(std::unique_ptr<Boardcore::Wiz5500> wiz5500)
@@ -74,7 +69,10 @@ bool EthernetBase::start(std::unique_ptr<Boardcore::Wiz5500> wiz5500)
     this->wiz5500 = std::move(wiz5500);
 
     // Reset the device
-    this->wiz5500->reset();
+    if (!this->wiz5500->reset())
+    {
+        return false;
+    }
 
     // Setup ip and other stuff
     this->wiz5500->setSubnetMask(SUBNET);
@@ -82,43 +80,59 @@ bool EthernetBase::start(std::unique_ptr<Boardcore::Wiz5500> wiz5500)
     this->wiz5500->setSourceIp(genNewRandomIp());
     this->wiz5500->setSourceMac(genNewRandomMac());
 
-    this->wiz5500->setOnIpConflict(
-        [this]() { this->wiz5500->setSourceIp(genNewRandomIp()); });
+    this->wiz5500->setOnIpConflict([this]() {
+        this->wiz5500->setSourceIp(genNewRandomIp());
+    });
 
     // Ok now open the UDP socket
-    if (!this->wiz5500->openUdp(0, RECV_PORT, {255, 255, 255, 255}, SEND_PORT,
-                                500))
+    if (!this->wiz5500->openUdp(0, RECV_PORT, {255, 255, 255, 255}, SEND_PORT, 500))
     {
         return false;
     }
 
-    auto mav_handler = [this](EthernetMavDriver* channel,
-                              const mavlink_message_t& msg) { handleMsg(msg); };
-
-    mav_driver = std::make_unique<EthernetMavDriver>(this, mav_handler, 0, 10);
-
-    if (!mav_driver->start())
+    if (!ActiveObject::start())
     {
         return false;
     }
 
+    started = true;
     return true;
 }
 
-void EthernetBase::handleMsg(const mavlink_message_t& msg)
+void EthernetBase::run()
 {
-    // Dispatch the message through the hub.
-    ModuleManager::getInstance().get<HubBase>()->dispatchOutgoingMsg(msg);
-}
+    mavlink_message_t msg;
+    mavlink_status_t status;
+    uint8_t msg_buf[256];
 
-ssize_t EthernetBase::receive(uint8_t* pkt, size_t max_len)
-{
-    WizIp dst_ip;
-    uint16_t dst_port;
-    return wiz5500->recvfrom(0, pkt, max_len, dst_ip, dst_port);
-}
+    while (!shouldStop())
+    {
+        WizIp dst_ip;
+        uint16_t dst_port;
+        ssize_t rcv_len = this->wiz5500->recvfrom(0, msg_buf, sizeof(msg_buf),
+                                                  dst_ip, dst_port, 500);
 
-bool EthernetBase::send(uint8_t* pkt, size_t len)
-{
-    return wiz5500->send(0, pkt, len, 100);
+        if (rcv_len == -1)
+        {
+            // Avoid spin looping, failure are highly likely to happen in
+            // sequence
+            Thread::sleep(100);
+        }
+        else
+        {
+            for (ssize_t i = 0; i < rcv_len; i++)
+            {
+                uint8_t parse_result = mavlink_parse_char(
+                    MAVLINK_COMM_0, msg_buf[i], &msg, &status);
+
+                if (parse_result == 1)
+                {
+                    // Dispatch the message through the hub.
+                    ModuleManager::getInstance()
+                        .get<HubBase>()
+                        ->dispatchOutgoingMsg(msg);
+                }
+            }
+        }
+    }
 }
