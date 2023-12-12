53575418b9c6af32b5de1bfc1ce895512f241477
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2023-12-12 11:16:09 +0100

    [BoardStatus] Updated mavlink library and created BoardStatus to send ARP_TM periodically

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -137,8 +137,8 @@ set(GROUNDSTATION_BASE
 )
 
 set(GROUNDSTATION_AUTOMATED
+    src/boards/Groundstation/Automated/BoardStatus.cpp
     src/boards/Groundstation/Automated/Radio/Radio.cpp
-    src/boards/Groundstation/Automated/Radio/RadioStatus.cpp
     src/boards/Groundstation/Automated/Follower/Follower.cpp
     src/boards/Groundstation/Automated/Ports/Ethernet.cpp
     src/boards/Groundstation/Automated/Hub.cpp
