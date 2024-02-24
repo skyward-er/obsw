2b9a21cb1f531f320f15012782dcc818b28c40ee
Federico Lolli <federico.lolli@skywarder.eu>
2024-02-24 11:23:44 +0100

    [ARP] Filled state machine mathods

--- src/boards/Groundstation/Automated/SMController/SMController.h
+++ src/boards/Groundstation/Automated/SMController/SMController.h
@@ -39,53 +39,21 @@ public:
     SMController();
 
     // FSM States
-
-    // Super state for the feedback loop
     Boardcore::State state_feedback(const Boardcore::Event& event);
-
-    // Super state for the no-feedback loop
     Boardcore::State state_no_feedback(const Boardcore::Event& event);
-
-    // Initial state
     Boardcore::State state_init(const Boardcore::Event& event);
-
-    // Error state reached if errors arise in init
     Boardcore::State state_init_error(const Boardcore::Event& event);
-
-    // State of successful initialization
     Boardcore::State state_init_done(const Boardcore::Event& event);
-
-    // State for tuning settings of the no-feedback loop
     Boardcore::State insert_info(const Boardcore::Event& event);
-
-    // Feedback state of system armed
     Boardcore::State state_armed(const Boardcore::Event& event);
-
-    // Feedback state of testing
     Boardcore::State state_test(const Boardcore::Event& event);
-
-    // Feedback state of calibration for auto-tuning
     Boardcore::State state_calibrate(const Boardcore::Event& event);
-
-    // Feedback state for antennas fix received
     Boardcore::State state_fix_antennas(const Boardcore::Event& event);
-
-    // Feedback state for rocket fix received
     Boardcore::State state_fix_rocket(const Boardcore::Event& event);
-
-    // Feedback state for active control loop
     Boardcore::State state_active(const Boardcore::Event& event);
-
-    // No-feedback state of system armed
     Boardcore::State state_armed_nf(const Boardcore::Event& event);
-
-    // No-feedback state of testing
     Boardcore::State state_test_nf(const Boardcore::Event& event);
-
-    // No-feedback state for rocket fix received
     Boardcore::State state_fix_rocket_nf(const Boardcore::Event& event);
-
-    // No-feedback state for active control loop
     Boardcore::State state_active_nf(const Boardcore::Event& event);
 
 private:
