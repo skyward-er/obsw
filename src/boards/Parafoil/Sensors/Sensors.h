c7056f23850b6f6a5293591cb43b7116a4f69bdb
Federico Mandelli <federico.mandelli@skywarder.eu>
2024-07-18 10:48:00 +0200

    [Copyright] Fixed wrong copyright that generated warnings in pipeline

--- src/boards/Parafoil/Sensors/Sensors.h
+++ src/boards/Parafoil/Sensors/Sensors.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2023 Skyward Experimental Rocketry
- * Author: Matteo Pignataro, Federico Mandelli
+ * Authors: Matteo Pignataro, Federico Mandelli
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
