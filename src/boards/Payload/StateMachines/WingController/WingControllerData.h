645c8187adbdf6a8be99261bb9e3d5f3ba84fc63
Niccolò Betto <niccolo.betto@skywarder.eu>
2024-09-10 15:23:08 +0200

    [Payload] Fix authors and remove unused file

--- src/boards/Payload/StateMachines/WingController/WingControllerData.h
+++ src/boards/Payload/StateMachines/WingController/WingControllerData.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2024 Skyward Experimental Rocketry
- * Author: Federico Mandelli, Niccolò Betto
+ * Authors: Federico Mandelli, Niccolò Betto
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
