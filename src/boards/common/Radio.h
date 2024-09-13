56c907af416e045db15771b927e2216141fb89c1
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2024-09-13 19:05:55 +0200

    [Radio] Updated main radio config to 420,69 MHz

--- src/boards/common/Radio.h
+++ src/boards/common/Radio.h
@@ -29,7 +29,7 @@ namespace Common
 {
 
 static constexpr Boardcore::SX1278Fsk::Config MAIN_RADIO_CONFIG = {
-    .freq_rf    = 419000000,
+    .freq_rf    = 420690000,
     .freq_dev   = 50000,
     .bitrate    = 48000,
     .rx_bw      = Boardcore::SX1278Fsk::Config::RxBw::HZ_125000,
