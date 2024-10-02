5c5ff9b50d828ec5a321ff22b18e3da3c5535c1f
Davide Mor <tazdevil971@gmail.com>
2024-10-02 15:17:03 +0200

    [gs] Added nack on failed packet dispatch

--- src/boards/Gs/Hub.cpp
+++ src/boards/Gs/Hub.cpp
@@ -22,34 +22,53 @@
 
 #include "Hub.h"
 
+#include <Gs/Ports/Serial.h>
 #include <Gs/Radio/Radio.h>
 #include <Gs/Radio/RadioStatus.h>
-#include <Gs/Ports/Serial.h>
 
 using namespace Gs;
 using namespace Boardcore;
 
-void Hub::dispatchOutgoingMsg(const mavlink_message_t& msg) {
-    RadioStatus *status = ModuleManager::getInstance().get<RadioStatus>();
+void Hub::dispatchOutgoingMsg(const mavlink_message_t& msg)
+{
+    RadioStatus* status = ModuleManager::getInstance().get<RadioStatus>();
 
     // TODO: Dispatch to correct radio using mavlink ids
 
+    bool send_ok = false;
+
     if (status->isMainRadioPresent())
     {
-        RadioMain *radio = ModuleManager::getInstance().get<RadioMain>();
-        radio->sendMsg(msg);
+        RadioMain* radio = ModuleManager::getInstance().get<RadioMain>();
+        send_ok |= radio->sendMsg(msg);
     }
 
     if (status->isPayloadRadioPresent())
     {
-        RadioPayload *radio = ModuleManager::getInstance().get<RadioPayload>();
-        radio->sendMsg(msg);
+        RadioPayload* radio = ModuleManager::getInstance().get<RadioPayload>();
+        send_ok |= radio->sendMsg(msg);
+    }
+
+    // If both of the sends went wrong, just send a nack
+    if (!send_ok)
+    {
+        sendNack(msg);
     }
 }
 
-void Hub::dispatchIncomingMsg(const mavlink_message_t& msg) {
+void Hub::dispatchIncomingMsg(const mavlink_message_t& msg)
+{
     Serial* serial = ModuleManager::getInstance().get<Serial>();
     serial->sendMsg(msg);
 
     // TODO: Add UDP dispatch
+}
+
+void Hub::sendNack(const mavlink_message_t& msg)
+{
+    mavlink_message_t nack_msg;
+    mavlink_msg_nack_tm_pack(GS_SYSTEM_ID, GS_COMPONENT_ID, &nack_msg,
+                             msg.msgid, msg.seq);
+
+    dispatchIncomingMsg(nack_msg);
 }
