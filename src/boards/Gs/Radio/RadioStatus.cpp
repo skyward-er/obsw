b88713cb9532e48c89ee868e256a59681ecc2148
Davide Mor <tazdevil971@gmail.com>
2024-10-02 15:17:03 +0200

    [gs] Added gs telemetry

--- src/boards/Gs/Radio/RadioStatus.cpp
+++ src/boards/Gs/Radio/RadioStatus.cpp
@@ -22,18 +22,86 @@
 
 #include "RadioStatus.h"
 
+#include <Gs/Config/GeneralConfig.h>
+#include <Gs/Hub.h>
+#include <common/Mavlink.h>
+#include <drivers/timer/TimestampTimer.h>
+
 using namespace Boardcore;
 using namespace Gs;
 
-bool Gs::RadioStatus::isMainRadioPresent() { return main_radio_present; }
-bool Gs::RadioStatus::isPayloadRadioPresent() { return payload_radio_present; }
+bool RadioStatus::isMainRadioPresent() { return main_radio_present; }
+bool RadioStatus::isPayloadRadioPresent() { return payload_radio_present; }
 
-void Gs::RadioStatus::setMainRadioPresent(bool present)
+bool RadioStatus::start()
+{
+    if (!ActiveObject::start())
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void RadioStatus::setMainRadioPresent(bool present)
 {
     main_radio_present = present;
 }
 
-void Gs::RadioStatus::setPayloadRadioPresent(bool present)
+void RadioStatus::setPayloadRadioPresent(bool present)
 {
     payload_radio_present = present;
+}
+
+void RadioStatus::run()
+{
+    while (!shouldStop())
+    {
+        miosix::Thread::sleep(RADIO_STATUS_PERIOD);
+
+        mavlink_receiver_tm_t tm = {0};
+        tm.timestamp             = TimestampTimer::getTimestamp();
+
+        if (main_radio_present)
+        {
+            tm.main_radio_present = 1;
+
+            auto stats =
+                ModuleManager::getInstance().get<RadioMain>()->getStats();
+            tm.main_packet_tx_error_count = stats.send_errors;
+            tm.main_tx_bitrate = main_tx_bitrate.update(stats.bits_tx_count);
+            tm.main_packet_rx_success_count = stats.packet_rx_success_count;
+            tm.main_packet_rx_drop_count    = stats.packet_rx_drop_count;
+            tm.main_rx_bitrate = main_rx_bitrate.update(stats.bits_rx_count);
+            tm.main_rx_rssi    = stats.rx_rssi;
+            tm.main_rx_fei     = stats.rx_fei;
+
+            last_main_stats = stats;
+        }
+
+        if (payload_radio_present)
+        {
+            tm.payload_radio_present = 1;
+
+            auto stats =
+                ModuleManager::getInstance().get<RadioPayload>()->getStats();
+            tm.payload_packet_tx_error_count = stats.send_errors;
+            tm.payload_tx_bitrate =
+                payload_tx_bitrate.update(stats.bits_tx_count);
+            tm.payload_packet_rx_success_count = stats.packet_rx_success_count;
+            tm.payload_packet_rx_drop_count    = stats.packet_rx_drop_count;
+            tm.payload_rx_bitrate =
+                payload_rx_bitrate.update(stats.bits_rx_count);
+            tm.payload_rx_rssi = stats.rx_rssi;
+            tm.payload_rx_fei  = stats.rx_fei;
+
+            last_payload_stats = stats;
+        }
+
+        mavlink_message_t msg;
+        mavlink_msg_receiver_tm_encode(GS_SYSTEM_ID, GS_COMPONENT_ID, &msg,
+                                       &tm);
+
+        ModuleManager::getInstance().get<Hub>()->dispatchIncomingMsg(msg);
+    }
 }
