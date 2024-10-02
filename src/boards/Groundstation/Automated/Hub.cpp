a1b11c80b4eeec711304d78f6d71d75afbe8b740
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2024-10-02 15:17:10 +0200

    [ARP] Updated message dispatching and sniffing
    - If the destination is the ARP system, we won't forward the message via Radio.
    - Now we also handle the logging info request on the ARP system

--- src/boards/Groundstation/Automated/Hub.cpp
+++ src/boards/Groundstation/Automated/Hub.cpp
@@ -46,168 +46,178 @@ using namespace miosix;
 void Hub::dispatchOutgoingMsg(const mavlink_message_t& msg)
 {
     // TODO: Dispatch to correct radio using mavlink ids
-
     bool send_ok           = false;
     ModuleManager& modules = ModuleManager::getInstance();
 
     RadioMain* radio = modules.get<RadioMain>();
 
-    switch (msg.msgid)
+    if (msg.sysid == MAV_SYSID_ARP)
     {
-        case MAVLINK_MSG_ID_ARP_COMMAND_TC:
+        switch (msg.msgid)
         {
-            // Create the map between the commands and the corresponding events
-            static const std::map<MavArpCommandList, Events> commandToEvent{
-                {MAV_ARP_CMD_FORCE_INIT, TMTC_ARP_FORCE_INIT},
-                {MAV_ARP_CMD_RESET_ALGORITHM, TMTC_ARP_RESET_ALGORITHM},
-                {MAV_ARP_CMD_RESET_BOARD, TMTC_ARP_RESET_BOARD},
-                {MAV_ARP_CMD_FORCE_NO_FEEDBACK, TMTC_ARP_FORCE_NO_FEEDBACK},
-                {MAV_ARP_CMD_ARM, TMTC_ARP_ARM},
-                {MAV_ARP_CMD_DISARM, TMTC_ARP_DISARM},
-                {MAV_ARP_CMD_FOLLOW, TMTC_ARP_FOLLOW},
-                {MAV_ARP_CMD_CALIBRATE, TMTC_ARP_CALIBRATE},
-                {MAV_ARP_CMD_ENTER_TEST_MODE, TMTC_ARP_ENTER_TEST_MODE},
-                {MAV_ARP_CMD_EXIT_TEST_MODE, TMTC_ARP_EXIT_TEST_MODE},
-            };
-
-            MavArpCommandList commandId = static_cast<MavArpCommandList>(
-                mavlink_msg_arp_command_tc_get_command_id(&msg));
-
-            auto it = commandToEvent.find(commandId);
-
-            if (it != commandToEvent.end())
+             case MAVLINK_MSG_ID_ARP_COMMAND_TC:
             {
-                EventBroker::getInstance().post(it->second, TOPIC_TMTC);
-            }
-            else
-            {
-                return sendNack(msg);
-            }
+                // Create the map between the commands and the corresponding events
+                static const std::map<MavArpCommandList, Events> commandToEvent{
+                    {MAV_ARP_CMD_FORCE_INIT, TMTC_ARP_FORCE_INIT},
+                    {MAV_ARP_CMD_RESET_ALGORITHM, TMTC_ARP_RESET_ALGORITHM},
+                    {MAV_ARP_CMD_RESET_BOARD, TMTC_ARP_RESET_BOARD},
+                    {MAV_ARP_CMD_FORCE_NO_FEEDBACK, TMTC_ARP_FORCE_NO_FEEDBACK},
+                    {MAV_ARP_CMD_ARM, TMTC_ARP_ARM},
+                    {MAV_ARP_CMD_DISARM, TMTC_ARP_DISARM},
+                    {MAV_ARP_CMD_FOLLOW, TMTC_ARP_FOLLOW},
+                    {MAV_ARP_CMD_CALIBRATE, TMTC_ARP_CALIBRATE},
+                    {MAV_ARP_CMD_ENTER_TEST_MODE, TMTC_ARP_ENTER_TEST_MODE},
+                    {MAV_ARP_CMD_EXIT_TEST_MODE, TMTC_ARP_EXIT_TEST_MODE},
+                };
 
-            // Acknowledge the message
-            sendAck(msg);
-            break;
-        }
-        case MAVLINK_MSG_ID_SET_STEPPER_ANGLE_TC:
-        {
-            StepperList stepperId = static_cast<StepperList>(
-                mavlink_msg_set_stepper_angle_tc_get_stepper_id(&msg));
-            float angle = mavlink_msg_set_stepper_angle_tc_get_angle(&msg);
+                MavArpCommandList commandId = static_cast<MavArpCommandList>(
+                    mavlink_msg_arp_command_tc_get_command_id(&msg));
 
-            // The stepper is moved of 'angle' degrees
-            ErrorMovement moved =
-                modules.get<SMA>()->moveStepperDeg(stepperId, angle);
-            if (moved == ErrorMovement::OK)
-                sendAck(msg);
-            else
-                sendNack(msg);
-            break;
-        }
-        case MAVLINK_MSG_ID_SET_STEPPER_STEPS_TC:
-        {
+                auto it = commandToEvent.find(commandId);
 
-            StepperList stepperId = static_cast<StepperList>(
-                mavlink_msg_set_stepper_steps_tc_get_stepper_id(&msg));
-            int16_t steps = mavlink_msg_set_stepper_steps_tc_get_steps(&msg);
-
-            // The stepper is moved of 'steps' steps
-            ErrorMovement moved =
-                modules.get<SMA>()->moveStepperSteps(stepperId, steps);
-            if (moved == ErrorMovement::OK)
-                sendAck(msg);
-            else
-                sendNack(msg);
-            break;
-        }
-        case MAVLINK_MSG_ID_SET_ROCKET_COORDINATES_ARP_TC:
-        {
-            float latitude =
-                mavlink_msg_set_rocket_coordinates_arp_tc_get_latitude(&msg);
-            float longitude =
-                mavlink_msg_set_rocket_coordinates_arp_tc_get_longitude(&msg);
-            float height =
-                mavlink_msg_set_rocket_coordinates_arp_tc_get_height(&msg);
-
-            GPSData gpsData;
-            gpsData.latitude   = latitude;
-            gpsData.longitude  = longitude;
-            gpsData.height     = height;
-            gpsData.fix        = 3;
-            gpsData.satellites = 42;
-
-            modules.get<SMA>()->setInitialRocketCoordinates(gpsData);
-            sendAck(msg);
-            break;
-        }
-        case MAVLINK_MSG_ID_SET_ANTENNA_COORDINATES_ARP_TC:
-        {
-            float latitude =
-                mavlink_msg_set_antenna_coordinates_arp_tc_get_latitude(&msg);
-            float longitude =
-                mavlink_msg_set_antenna_coordinates_arp_tc_get_longitude(&msg);
-            float height =
-                mavlink_msg_set_antenna_coordinates_arp_tc_get_height(&msg);
-
-            GPSData gpsData;
-            gpsData.latitude   = latitude;
-            gpsData.longitude  = longitude;
-            gpsData.height     = height;
-            gpsData.fix        = 3;
-            gpsData.satellites = 42;
-
-            modules.get<SMA>()->setAntennaCoordinates(gpsData);
-            sendAck(msg);
-            break;
-        }
-        case MAVLINK_MSG_ID_SYSTEM_TM_REQUEST_TC:
-        {
-            mavlink_message_t msg_response;
-            SystemTMList tmId = static_cast<SystemTMList>(
-                mavlink_msg_system_tm_request_tc_get_tm_id(&msg));
-
-            // If it is handled create the msg_response, otherwise return
-            // NACK
-            switch (tmId)
-            {
-                case SystemTMList::MAV_LOGGER_ID:
+                if (it != commandToEvent.end())
                 {
-                    mavlink_logger_tm_t tm;
-
-                    LoggerStats stats = Logger::getInstance().getStats();
-
-                    tm.timestamp          = TimestampTimer::getTimestamp();
-                    tm.log_number         = stats.logNumber;
-                    tm.too_large_samples  = stats.tooLargeSamples;
-                    tm.dropped_samples    = stats.droppedSamples;
-                    tm.queued_samples     = stats.queuedSamples;
-                    tm.buffers_filled     = stats.buffersFilled;
-                    tm.buffers_written    = stats.buffersWritten;
-                    tm.writes_failed      = stats.writesFailed;
-                    tm.last_write_error   = stats.lastWriteError;
-                    tm.average_write_time = stats.averageWriteTime;
-                    tm.max_write_time     = stats.maxWriteTime;
-
-                    mavlink_msg_logger_tm_encode(SysIDs::MAV_SYSID_ARP,
-                                                 Groundstation::GS_COMPONENT_ID,
-                                                 &msg_response, &tm);
-
-                    break;
+                    EventBroker::getInstance().post(it->second, TOPIC_TMTC);
                 }
-                default:
+                else
                 {
+                    return sendNack(msg);
+                }
+
+                // Acknowledge the message
+                sendAck(msg);
+                break;
+            }           
+            case MAVLINK_MSG_ID_SET_STEPPER_ANGLE_TC:
+            {
+                StepperList stepperId = static_cast<StepperList>(
+                    mavlink_msg_set_stepper_angle_tc_get_stepper_id(&msg));
+                float angle = mavlink_msg_set_stepper_angle_tc_get_angle(&msg);
+
+                // The stepper is moved of 'angle' degrees
+                ErrorMovement moved =
+                    modules.get<SMA>()->moveDeg(stepperId, angle);
+                if (moved == ErrorMovement::OK)
+                    sendAck(msg);
+                else
                     sendNack(msg);
-                    return;
-                }
+                break;
             }
+            case MAVLINK_MSG_ID_SET_STEPPER_STEPS_TC:
+            {
+                StepperList stepperId = static_cast<StepperList>(
+                    mavlink_msg_set_stepper_steps_tc_get_stepper_id(&msg));
+                int16_t steps =
+                    mavlink_msg_set_stepper_steps_tc_get_steps(&msg);
 
-            // Dispatching the created response message and then returning
-            // the ACK
-            dispatchIncomingMsg(msg_response);
-            sendAck(msg);
-            return;
+                // The stepper is moved of 'steps' steps
+                ErrorMovement moved =
+                    modules.get<SMA>()->move(stepperId, steps);
+                if (moved == ErrorMovement::OK)
+                    sendAck(msg);
+                else
+                    sendNack(msg);
+                break;
+            }
+            case MAVLINK_MSG_ID_SET_ROCKET_COORDINATES_ARP_TC:
+            {
+                float latitude =
+                    mavlink_msg_set_rocket_coordinates_arp_tc_get_latitude(
+                        &msg);
+                float longitude =
+                    mavlink_msg_set_rocket_coordinates_arp_tc_get_longitude(
+                        &msg);
+                float height =
+                    mavlink_msg_set_rocket_coordinates_arp_tc_get_height(&msg);
+
+                GPSData gpsData;
+                gpsData.latitude   = latitude;
+                gpsData.longitude  = longitude;
+                gpsData.height     = height;
+                gpsData.fix        = 3;
+                gpsData.satellites = 42;
+
+                modules.get<SMA>()->setInitialRocketCoordinates(gpsData);
+                sendAck(msg);
+                break;
+            }
+            case MAVLINK_MSG_ID_SET_ANTENNA_COORDINATES_ARP_TC:
+            {
+                float latitude =
+                    mavlink_msg_set_antenna_coordinates_arp_tc_get_latitude(
+                        &msg);
+                float longitude =
+                    mavlink_msg_set_antenna_coordinates_arp_tc_get_longitude(
+                        &msg);
+                float height =
+                    mavlink_msg_set_antenna_coordinates_arp_tc_get_height(&msg);
+
+                GPSData gpsData;
+                gpsData.latitude   = latitude;
+                gpsData.longitude  = longitude;
+                gpsData.height     = height;
+                gpsData.fix        = 3;
+                gpsData.satellites = 42;
+
+                modules.get<SMA>()->setAntennaCoordinates(gpsData);
+                sendAck(msg);
+                break;
+            }
+            case MAVLINK_MSG_ID_SYSTEM_TM_REQUEST_TC:
+            {
+                mavlink_message_t msg_response;
+                SystemTMList tmId = static_cast<SystemTMList>(
+                    mavlink_msg_system_tm_request_tc_get_tm_id(&msg));
+
+                // If it is handled create the msg_response, otherwise return
+                // NACK
+                switch (tmId)
+                {
+                    case SystemTMList::MAV_LOGGER_ID:
+                    {
+                        mavlink_logger_tm_t tm;
+
+                        LoggerStats stats = Logger::getInstance().getStats();
+
+                        tm.timestamp          = TimestampTimer::getTimestamp();
+                        tm.log_number         = stats.logNumber;
+                        tm.too_large_samples  = stats.tooLargeSamples;
+                        tm.dropped_samples    = stats.droppedSamples;
+                        tm.queued_samples     = stats.queuedSamples;
+                        tm.buffers_filled     = stats.buffersFilled;
+                        tm.buffers_written    = stats.buffersWritten;
+                        tm.writes_failed      = stats.writesFailed;
+                        tm.last_write_error   = stats.lastWriteError;
+                        tm.average_write_time = stats.averageWriteTime;
+                        tm.max_write_time     = stats.maxWriteTime;
+
+                        mavlink_msg_logger_tm_encode(
+                            SysIDs::MAV_SYSID_ARP,
+                            Groundstation::GS_COMPONENT_ID, &msg_response, &tm);
+
+                        break;
+                    }
+                    default:
+                    {
+                        sendNack(msg);
+                        return;
+                    }
+                }
+
+                // Dispatching the created response message and then returning
+                // the ACK
+                dispatchIncomingMsg(msg_response);
+                sendAck(msg);
+                return;
+            }
+            default:
+            {
+                sendNack(msg);
+                return;
+            }
         }
     }
-
     send_ok |= radio->sendMsg(msg);
 
     // If both of the sends went wrong, just send a nack
