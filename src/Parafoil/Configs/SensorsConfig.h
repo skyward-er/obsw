ec3e1e5d6ac317d0173154c5c554b789a80893b7
Davide Basso <davide.basso@skywarder.eu>
2025-04-15 20:16:41 +0200

    [Parafoil] Fix GPS sampling time

--- src/Parafoil/Configs/SensorsConfig.h
+++ src/Parafoil/Configs/SensorsConfig.h
@@ -87,7 +87,7 @@ constexpr auto ODR           = Boardcore::LPS22DF::ODR_100;
 namespace UBXGPS
 {
 constexpr auto ENABLED       = true;
-constexpr auto SAMPLING_RATE = 10_khz;
+constexpr auto SAMPLING_RATE = 10_hz;
 }  // namespace UBXGPS
 
 namespace ADS131M08
