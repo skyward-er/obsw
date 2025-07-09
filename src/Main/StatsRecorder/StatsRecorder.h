eef95667103a5f141e394a27914200d8eec9edea
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2025-07-09 09:43:46 +0200

    [Algorithms] Changed deployment and apogee altitude calculation
    Instead of saving the agl or msl altitude of all three ada algorithms
    we now calculate the average of the values from the algorithms that
     have reached consensus

--- src/Main/StatsRecorder/StatsRecorder.h
+++ src/Main/StatsRecorder/StatsRecorder.h
@@ -62,9 +62,7 @@ public:
         uint64_t apogeeTs = 0;
         float apogeeLat   = 0.0f;
         float apogeeLon   = 0.0f;
-        float apogeeAlt0  = 0.0f;
-        float apogeeAlt1  = 0.0f;
-        float apogeeAlt2  = 0.0f;
+        float apogeeAlt   = 0.0f;
 
         // Maximum acceleration after apogee
         uint64_t apogeeMaxAccTs = 0;
@@ -72,9 +70,7 @@ public:
 
         // Deployment
         uint64_t dplTs = 0;
-        float dplAlt0  = 0.0f;
-        float dplAlt1  = 0.0f;
-        float dplAlt2  = 0.0f;
+        float dplAlt   = 0.0f;
 
         // Maximum acceleration after deployment
         uint64_t dplMaxAccTs = 0;
@@ -93,9 +89,8 @@ public:
 
     void liftoffDetected(uint64_t ts);
     void shutdownDetected(uint64_t ts, float alt);
-    void apogeeDetected(uint64_t ts, float lat, float lon, float alt0,
-                        float alt1, float alt2);
-    void deploymentDetected(uint64_t ts, float alt0, float alt1, float alt2);
+    void apogeeDetected(uint64_t ts, float lat, float lon, float alt);
+    void deploymentDetected(uint64_t ts, float alt);
 
     void updateAcc(const Boardcore::AccelerometerData& data);
     void updateNas(const Boardcore::NASState& data);
