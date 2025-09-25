57c2501e3c08edef273ebb5a076028eedd11fea1
Niccolò Betto <niccolo.betto@skywarder.eu>
2025-09-25 13:35:11 +0200

    [Main] Increase cut duration to 1s
    Just in case 500ms are not enough, as seen in the Roccaraso flight.

--- src/Main/Configs/FMMConfig.h
+++ src/Main/Configs/FMMConfig.h
@@ -36,7 +36,7 @@ constexpr auto MISSION_TIMEOUT          = 15min;
 constexpr auto ENGINE_SHUTDOWN_TIMEOUT  = 2600ms;
 constexpr auto APOGEE_TIMEOUT           = 19s;
 constexpr auto NITROGEN_VENTING_TIMEOUT = 150s;  // 120s + 25% safety margin
-constexpr auto CUT_DURATION             = 500ms;
+constexpr auto CUT_DURATION             = 1000ms;
 
 }  // namespace FlightModeManager
 }  // namespace Config
