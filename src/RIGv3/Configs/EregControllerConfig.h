bd87828083a98c7303150ebee28ee7f9469a3684
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2026-03-17 16:31:48 +0100

    [RIGv3] Added electronic regulator algorithm controllers for Ox and Fuel
    After several cft tests over the course of hydra cft campaign this version of the algorithm was deemed to be reliable,
    the algorithm has a PID at its core that computes the CV needed to reach a target pressure, the output of the PID is then
    converted to a servo command (in the range [0;1]) and used to actuate a servo-controlled ball valve. The algorithms uses
    both the upstream (prz tank) and the downstream (ox or fuel tank) pressures as the input for the PID.
    
    Furthermore, in order to mitigate the initial loss of pressure caused by the opening of the main valves a method to
    inject in the PID an initial integrative error was added, this allows us to mitigate the offset caused by the initial
    pressure loss.

---
+++ src/RIGv3/Configs/EregControllerConfig.h
@@ -0,0 +1,132 @@
+/* Copyright (c) 2025 Skyward Experimental Rocketry
+ * Author: Pietro Bortolus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#pragma once
+
+#include <RIGv3/Actuators/Actuators.h>
+#include <algorithms/Ereg/EregConfig.h>
+#include <units/Frequency.h>
+
+#include <chrono>
+
+namespace RIGv3
+{
+namespace Config
+{
+namespace ERegFuel
+{
+
+using namespace Boardcore::Units::Frequency;
+using namespace std::chrono;
+
+// why is this not static constexpr?
+constexpr ServosList EREG_SERVO = ServosList::PRZ_FUEL_VALVE;
+
+constexpr float PRESSURE_THRESHOLD = 0.01f;  // [Bar]
+
+constexpr float TARGET_PRESSURE      = 58.0f;  // [Bar]
+constexpr float PILOT_FLAME_INTEGRAL = 0.0f;
+constexpr float RAMPUP_INTEGRAL      = 0.0f;
+constexpr Hertz UPDATE_RATE          = 100_hz;
+constexpr int FILTER_SAMPLES         = 3;
+
+constexpr float UPDATE_RATE_SECONDS = 1 / UPDATE_RATE.value();  // [s]
+
+const static Boardcore::EregPIDConfig STABILIZING_CONFIG = {
+    .KP = 0.004f,
+    .KI = 0.0f,
+    .KD = 0.0f,
+
+    .Ts = UPDATE_RATE_SECONDS,
+};
+
+const static Boardcore::EregPIDConfig DISCHARGING_CONFIG = {
+    .KP = 0.13f,
+    .KI = 0.0f,
+    .KD = 0.0f,
+
+    .Ts = UPDATE_RATE_SECONDS,
+};
+
+const static Boardcore::EregValveInfo VALVE_INFO = {
+    .minServoPosition = 0.11f,
+    .minValveAngle    = 12,
+    .maxCv            = 0.912291f,
+
+    .polyValveCoeff = {11.5576f, -27.1038f, 23.5596f, -9.4317f, 2.3962f,
+                       0.0032f},
+
+    .polyServoCoeff = {0.0f, 0.0f, 1.3302e-6f, -169.3787e-6f, 18.6759e-3f,
+                       11.8442e-3f},
+};
+
+}  // namespace ERegFuel
+
+namespace ERegOx
+{
+
+using namespace Boardcore::Units::Frequency;
+using namespace std::chrono;
+
+constexpr ServosList EREG_SERVO = ServosList::PRZ_OX_VALVE;
+
+constexpr float PRESSURE_THRESHOLD = 0.01f;  // [Bar]
+
+constexpr float TARGET_PRESSURE      = 58.0f;  // [Bar]
+constexpr float PILOT_FLAME_INTEGRAL = 0.0f;
+constexpr float RAMPUP_INTEGRAL      = 0.0f;
+constexpr Hertz UPDATE_RATE          = 100_hz;
+constexpr int FILTER_SAMPLES         = 3;
+
+constexpr float UPDATE_RATE_SECONDS = 1 / UPDATE_RATE.value();  // [s]
+
+const static Boardcore::EregPIDConfig STABILIZING_CONFIG = {
+    .KP = 0.01f,
+    .KI = 0.0f,
+    .KD = 0.0f,
+
+    .Ts = UPDATE_RATE_SECONDS,
+};
+
+const static Boardcore::EregPIDConfig DISCHARGING_CONFIG = {
+    .KP = 0.26f,
+    .KI = 0.0f,
+    .KD = 0.0f,
+
+    .Ts = UPDATE_RATE_SECONDS,
+};
+
+const static Boardcore::EregValveInfo VALVE_INFO = {
+    .minServoPosition = 0.1163f,
+    .minValveAngle    = 16,
+    .maxCv            = 0.981898f,
+
+    .polyValveCoeff = {9.7769f, -23.6167f, 21.5392f, -9.2101f, 2.4963f,
+                       0.0036f},
+
+    .polyServoCoeff = {1.3476e-9f, -258.0271e-9f, 18.2523e-6f, -577.5516e-6f,
+                       16.5135e-3f, 2.7180e-3f},
+};
+
+}  // namespace ERegOx
+}  // namespace Config
+}  // namespace RIGv3
+
---
+++ src/RIGv3/StateMachines/EregController/EregControllerOx.h
@@ -0,0 +1,91 @@
+/* Copyright (c) 2025 Skyward Experimental Rocketry
+ * Author: Pietro Bortolus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <RIGv3/Actuators/Actuators.h>
+#include <RIGv3/BoardScheduler.h>
+#include <RIGv3/Configs/EregControllerConfig.h>
+#include <RIGv3/StateMachines/EregController/EregControllerData.h>
+#include <algorithms/Ereg/Ereg.h>
+#include <algorithms/Ereg/EregData.h>
+#include <common/MeanFilter.h>
+#include <events/FSM.h>
+#include <utils/DependencyManager/DependencyManager.h>
+
+namespace RIGv3
+{
+
+class ERegControllerOx
+    : public Boardcore::FSM<ERegControllerOx>,
+      public Boardcore::InjectableWithDeps<BoardScheduler, Actuators, Sensors>
+{
+public:
+    ERegControllerOx();
+
+    [[nodiscard]] bool start() override;
+
+    ERegState getState();
+
+    void changePIDConfig(Boardcore::EregPIDConfig newPressurizationConfig,
+                         Boardcore::EregPIDConfig newDischargeConfig);
+
+    void changeTargetPressure(float newTargetPressure);
+    void setIntegralContribution(float newPilotContribution,
+                                 float newRampupContribution);
+
+private:
+    void update();
+
+    // FSM states
+    void state_init(const Boardcore::Event& event);
+    void state_closed(const Boardcore::Event& event);
+    void state_pressurizing(const Boardcore::Event& event);
+    void state_pilot_flame(const Boardcore::Event& event);
+    void state_rampup(const Boardcore::Event& event);
+
+    void updateAndLogStatus(ERegState state);
+
+    std::atomic<ERegState> state{ERegState::INIT};
+
+    Boardcore::Logger& sdLogger   = Boardcore::Logger::getInstance();
+    Boardcore::PrintLogger logger = Boardcore::Logging::getLogger("ereg");
+
+    Boardcore::Ereg regulator;
+    MeanFilter<float, Config::ERegOx::FILTER_SAMPLES> downstreamPressureFilter;
+    MeanFilter<float, Config::ERegOx::FILTER_SAMPLES> upstreamPressureFilter;
+
+    float lastDownstreamInput = 0.0f;  // Last sample used by ereg algorithm
+    float lastUpstreamInput   = 0.0f;  // Last sample used by ereg algorithm
+
+    Boardcore::EregPIDConfig pressurizationConfig =
+        Config::ERegOx::STABILIZING_CONFIG;
+    Boardcore::EregPIDConfig dischargeConfig =
+        Config::ERegOx::DISCHARGING_CONFIG;
+
+    float targetPressure = Config::ERegOx::TARGET_PRESSURE;
+
+    float pilotFlameIntegral = Config::ERegOx::PILOT_FLAME_INTEGRAL;
+    float rampupIntegral     = Config::ERegOx::RAMPUP_INTEGRAL;
+};
+
+}  // namespace RIGv3
