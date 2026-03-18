00dd73e12d9e714d698eb7862d1b8b76429710ce
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2026-03-18 16:00:28 +0100

    [RIGv3] Added missing sensors to Sensors class
    Some functionalities are still missing, like retreiving data via CAN, servo current consumption
    and others.

--- src/RIGv3/Configs/SensorsConfig.h
+++ src/RIGv3/Configs/SensorsConfig.h
@@ -61,11 +61,26 @@ namespace ADC_0
 {
 constexpr bool ENABLED = true;
 
+using Channel = Boardcore::ADS131M08Defs::Channel;
+
+constexpr auto PRZ_VESSEL_1_PT_CHANNEL = Channel::CHANNEL_0;
+constexpr auto PRZ_VESSEL_2_PT_CHANNEL = Channel::CHANNEL_1;
+constexpr auto PRZ_FILLING_PT_CHANNEL  = Channel::CHANNEL_2;
+constexpr auto OX_VESSEL_PT_CHANNEL    = Channel::CHANNEL_3;
+constexpr auto OX_FILLING_PT_CHANNEL   = Channel::CHANNEL_4;
+constexpr auto OX_VESSEL_LC_CHANNEL    = Channel::CHANNEL_5;
+
+constexpr float CH0_SHUNT_RESISTANCE = 29.283f;
+constexpr float CH1_SHUNT_RESISTANCE = 29.233f;
+constexpr float CH2_SHUNT_RESISTANCE = 29.268f;
+constexpr float CH3_SHUNT_RESISTANCE = 29.645f;
+constexpr float CH4_SHUNT_RESISTANCE = 29.708f;
+constexpr float CH5_SHUNT_RESISTANCE = 29.708f;
 }  // namespace ADC_0
 
 namespace ADC_1
 {
-constexpr bool ENABLED = true;
+constexpr bool ENABLED = false;
 
 }  // namespace ADC_1
 
@@ -73,11 +88,37 @@ namespace ADC_2
 {
 constexpr bool ENABLED = true;
 
+using Channel = Boardcore::ADS131M08Defs::Channel;
+
+constexpr auto PRZ_TANK_PT_CHANNEL        = Channel::CHANNEL_0;
+constexpr auto OX_REG_OUT_PT_CHANNEL      = Channel::CHANNEL_1;
+constexpr auto FUEL_REG_OUT_PT_CHANNEL    = Channel::CHANNEL_2;
+constexpr auto OX_TANK_PT_CHANNEL         = Channel::CHANNEL_3;
+constexpr auto FUEL_TANK_PT_CHANNEL       = Channel::CHANNEL_4;
+constexpr auto IGNITER_CHAMBER_PT_CHANNEL = Channel::CHANNEL_5;
+constexpr auto MAIN_CHAMBER_PT_CHANNEL    = Channel::CHANNEL_6;
+
+constexpr float CH0_SHUNT_RESISTANCE = 29.283f;
+constexpr float CH1_SHUNT_RESISTANCE = 29.233f;
+constexpr float CH2_SHUNT_RESISTANCE = 29.268f;
+constexpr float CH3_SHUNT_RESISTANCE = 29.645f;
+constexpr float CH4_SHUNT_RESISTANCE = 29.708f;
+constexpr float CH5_SHUNT_RESISTANCE = 29.708f;
+constexpr float CH6_SHUNT_RESISTANCE = 29.708f;
+
 }  // namespace ADC_2
 
 namespace ADC_3
 {
 constexpr bool ENABLED = true;
+
+using Channel = Boardcore::ADS131M08Defs::Channel;
+
+constexpr auto ROCKET_LC_CHANNEL         = Channel::CHANNEL_0;
+constexpr auto MAIN_OX_ENCODER_CHANNEL   = Channel::CHANNEL_1;
+constexpr auto MAIN_FUEL_ENCODER_CHANNEL = Channel::CHANNEL_2;
+constexpr auto PRZ_OX_ENCODER_CHANNEL    = Channel::CHANNEL_3;
+constexpr auto PRZ_FUEL_ENCODER_CHANNEL  = Channel::CHANNEL_4;
 
 }  // namespace ADC_3
 
@@ -86,6 +127,113 @@ namespace InternalADC
 constexpr bool ENABLED = true;
 constexpr Hertz PERIOD = 10_hz;
 }  // namespace InternalADC
+
+namespace Trafag
+{
+// Default shunt resistance, used before calibration or if it's out of bounds
+constexpr float DEFAULT_SHUNT_RESISTANCE = 29.5;
+// Bounds of the shunt resistance, outside of which the calibration is ignored
+constexpr float SHUNT_RESISTANCE_LOWER_BOUND = 28.5;
+constexpr float SHUNT_RESISTANCE_UPPER_BOUND = 30.0;
+
+constexpr auto CALIBRATE_SAMPLE_COUNT         = 10;
+constexpr auto CALIBRATE_WAIT_BETWEEN_SAMPLES = 100ms;
+
+// Current at 0 bar relative to atmospheric pressure
+constexpr float MIN_CURRENT = 4;  // [mA]
+// Current at MAX_PRESSURE bar relative to atmospheric pressure
+constexpr float MAX_CURRENT = 20;  // [mA]
+
+constexpr float PRZ_VESSEL_1_MAX_PRESSURE = 400;  // bar
+constexpr float PRZ_VESSEL_2_MAX_PRESSURE = 400;  // bar
+constexpr float PRZ_FILLING_MAX_PRESSURE  = 400;  // bar
+constexpr float OX_VESSEL_MAX_PRESSURE    = 100;  // bar
+constexpr float OX_FILLING_MAX_PRESSURE   = 100;  // bar
+
+constexpr float PRZ_TANK_MAX_PRESSURE        = 400;  // bar
+constexpr float REG_OUT_MAX_PRESSURE         = 250;  // bar
+constexpr float OX_TANK_MAX_PRESSURE         = 100;  // bar
+constexpr float FUEL_TANK_MAX_PRESSURE       = 400;  // bar
+constexpr float IGNITER_CHAMBER_MAX_PRESSURE = 400;  // bar
+constexpr float MAIN_CHAMBER_MAX_PRESSURE    = 400;  // bar
+
+}  // namespace Trafag
+
+namespace Encoder
+{
+// Default shunt resistance
+constexpr float DEFAULT_SHUNT_RESISTANCE = 29.5f;
+
+constexpr float FULLSCALE_VOLTAGE = 4.7f;
+
+constexpr int SENSOR_RESISTANCE = 31240;  // Ohm
+constexpr int CURRENT_GAIN      = 100;
+
+constexpr float MAX_ANGLE = 360.0f;  // degrees
+
+}  // namespace Encoder
+
+namespace LoadCell
+{
+constexpr auto CALIBRATE_SAMPLE_COUNT  = 10;
+constexpr auto CALIBRATE_SAMPLE_PERIOD = 40ms;
+
+/*
+// Rocket ramp loadcell calibration data
+constexpr float ROCKET_P0_VOLTAGE = -0.0004273;
+constexpr float ROCKET_P0_MASS    = 10.005;
+constexpr float ROCKET_P1_VOLTAGE = -0.0018125;
+constexpr float ROCKET_P1_MASS    = 40.290;
+*/
+
+// Static fire teststand loadcell calibration data
+// - 0 kg       V: -0.000275
+// - 5.072 kg   V: -0.0029
+// - 15.249 kg  V: -0.00816
+// - 25.300 kg  V: -0.013327
+// constexpr float ROCKET_P0_VOLTAGE = -0.0029;
+// constexpr float ROCKET_P0_MASS    = 5.072;
+// constexpr float ROCKET_P1_VOLTAGE = -0.013327;
+// constexpr float ROCKET_P1_MASS    = 25.300;
+
+// New Rocket ramp loadcell calibration data
+// Points obtained from measurement on the ramp, through regression. Check
+// https://docs.google.com/spreadsheets/d/1nQse2GMNQPRd9KqRvvtfn-WiyyxnLysz/edit?gid=1597496666#gid=1597496666
+constexpr float ROCKET_P0_VOLTAGE = -0.001689;
+constexpr float ROCKET_P0_MASS    = 10.307;
+constexpr float ROCKET_P1_VOLTAGE = -0.007356;
+constexpr float ROCKET_P1_MASS    = 45.768;
+
+/* OLD CALIBRATION DATA (before 07/09/2024, before new flipping)
+// LC Vessel sensor calibration data
+// - 1.866kg V: 0.00027
+// - 5.050kg V: 0.00073
+// - 6.916kg V: 0.00100000941
+constexpr float VESSEL_P0_VOLTAGE = 0.00027;
+constexpr float VESSEL_P0_MASS    = 1.866;
+constexpr float VESSEL_P1_VOLTAGE = 0.0010;
+constexpr float VESSEL_P1_MASS    = 6.916;
+*/
+
+// LC Vessel sensor calibration data (post 07/09/2024)
+// - 0kg      V: 0.000630177
+// - 4.985kg  V: 0.000470017 (−31125.124875125 kg/v)
+// - 10.177kg V: 0.00030895  (−31681,645689808 kg/v)
+constexpr float VESSEL_P0_VOLTAGE = 0.000470017;
+constexpr float VESSEL_P0_MASS    = 4.985;
+constexpr float VESSEL_P1_VOLTAGE = 0.00030895;
+constexpr float VESSEL_P1_MASS    = 10.177;
+
+/*
+// LC Vessel sensor calibration data (post 08/11/2024, old flipping)
+// - 0kg      V: -0.009553
+// - 8.720kg  V: -0.010958 (−31125.124875125 kg/v)
+constexpr float VESSEL_P0_VOLTAGE = -0.009553;
+constexpr float VESSEL_P0_MASS    = 0.0;
+constexpr float VESSEL_P1_VOLTAGE = -0.010958;
+constexpr float VESSEL_P1_MASS    = 8.720;
+*/
+}  // namespace LoadCell
 
 }  // namespace Sensors
 }  // namespace Config
--- src/RIGv3/Sensors/Sensors.h
+++ src/RIGv3/Sensors/Sensors.h
@@ -27,6 +27,9 @@
 #include <drivers/adc/InternalADC.h>
 #include <sensors/ADS131M08/ADS131M08.h>
 #include <sensors/SensorManager.h>
+#include <sensors/analog/AnalogEncoder.h>
+#include <sensors/analog/TrafagPressureSensor.h>
+#include <sensors/analog/TwoPointAnalogLoadCell.h>
 #include <utils/DependencyManager/DependencyManager.h>
 
 #include <atomic>
@@ -40,17 +43,110 @@ class Sensors : public Boardcore::InjectableWithDeps<Buses, BoardScheduler>
 {
 public:
     [[nodiscard]] bool start();
+
+    void calibrate();
+
+    void calibrateLoadcells();
+
+    void calibrateEncoders();
+
     bool isStarted();
 
+    // Getters for raw data coming from sensors
     Boardcore::InternalADCData getInternalADCLastSample();
     Boardcore::ADS131M08Data getADC0LastSample();
     Boardcore::ADS131M08Data getADC1LastSample();
     Boardcore::ADS131M08Data getADC2LastSample();
     Boardcore::ADS131M08Data getADC3LastSample();
 
+    // Getters for processed data
+    Boardcore::PressureData getPrzVessel1Pressure();
+    Boardcore::PressureData getPrzVessel2Pressure();
+    Boardcore::PressureData getPrzFillingPressure();
+    Boardcore::PressureData getOxVesselPressure();
+    Boardcore::PressureData getOxFillingPressure();
+
+    Boardcore::PressureData getPrzTankPressure();
+    Boardcore::PressureData getOxRegOutPressure();
+    Boardcore::PressureData getFuelRegOutPressure();
+    Boardcore::PressureData getOxTankPressure();
+    Boardcore::PressureData getFuelTankPressure();
+    Boardcore::PressureData getIgniterChamberPressure();
+    Boardcore::PressureData getMainChamberPressure();
+
+    Boardcore::LoadCellData getOxVesselWeight();
+    Boardcore::LoadCellData getRocketWeight();
+    Boardcore::LoadCellData getOxTankWeight();
+
+    Boardcore::ServoPositionData getMainOxPosition();
+    Boardcore::ServoPositionData getMainFuelPosition();
+    Boardcore::ServoPositionData getOxRegPosition();
+    Boardcore::ServoPositionData getFuelRegPosition();
+
+    Boardcore::CurrentData getUmbilicalCurrent();
+    Boardcore::CurrentData getServoCurrent();
+    Boardcore::VoltageData getBatteryVoltage();
+
     std::vector<Boardcore::SensorInfo> getSensorInfos();
 
 private:
+    void przVessel1PressureInit();
+    void przVessel1PressureCallback();
+
+    void przVessel2PressureInit();
+    void przVessel2PressureCallback();
+
+    void przFillingPressureInit();
+    void przFillingPressureCallback();
+
+    void oxVesselPressureInit();
+    void oxVesselPressureCallback();
+
+    void oxFillingPressureInit();
+    void oxFillingPressureCallback();
+
+    void oxRegOutPressureInit();
+    void oxRegOutPressureCallback();
+
+    void fuelRegOutPressureInit();
+    void fuelRegOutPressureCallback();
+
+    void przTankPressureInit();
+    void przTankPressureCallback();
+
+    void oxTankPressureInit();
+    void oxTankPressureCallback();
+
+    void fuelTankPressureInit();
+    void fuelTankPressureCallback();
+
+    void igniterChamberPressureInit();
+    void igniterChamberPressureCallback();
+
+    void mainChamberPressureInit();
+    void mainChamberPressureCallback();
+
+    void oxVesselWeightInit();
+    void oxVesselWeightCallback();
+
+    void rocketWeightInit();
+    void rocketWeightCallback();
+
+    void oxTankWeightInit();
+    void oxTankWeightCallback();
+
+    void mainOxPositionInit();
+    void mainOxPositionCallback();
+
+    void mainFuelPositionInit();
+    void mainFuelPositionCallback();
+
+    void oxRegPositionInit();
+    void oxRegPositionCallback();
+
+    void fuelRegPositionInit();
+    void fuelRegPositionCallback();
+
     void internalAdcInit();
     void internalAdcCallback();
 
@@ -73,11 +169,33 @@ private:
 
     std::atomic<bool> started{false};
 
+    // Digital sensors
     std::unique_ptr<Boardcore::InternalADC> internalAdc;
-    std::unique_ptr<Boardcore::ADS131M08> adc0;
-    std::unique_ptr<Boardcore::ADS131M08> adc1;
-    std::unique_ptr<Boardcore::ADS131M08> adc2;
-    std::unique_ptr<Boardcore::ADS131M08> adc3;
+    std::unique_ptr<Boardcore::ADS131M08> adc0;  // Adc for the cart
+    std::unique_ptr<Boardcore::ADS131M08> adc1;  // Adc for the cart
+    std::unique_ptr<Boardcore::ADS131M08> adc2;  // Adc for the test stand
+    std::unique_ptr<Boardcore::ADS131M08> adc3;  // Adc for the test stand
+
+    // Analog sensors
+    std::unique_ptr<Boardcore::TrafagPressureSensor> przVessel1Pressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> przVessel2Pressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> przFillingPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> oxVesselPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> oxFillingPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> przTankPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> oxRegOutPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> fuelRegOutPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> oxTankPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> fuelTankPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> igniterChamberPressure;
+    std::unique_ptr<Boardcore::TrafagPressureSensor> mainChamberPressure;
+    std::unique_ptr<Boardcore::TwoPointAnalogLoadCell> oxVesselWeight;
+    std::unique_ptr<Boardcore::TwoPointAnalogLoadCell> rocketWeight;
+    std::unique_ptr<Boardcore::TwoPointAnalogLoadCell> oxTankWeight;
+    std::unique_ptr<Boardcore::AnalogEncoder> mainOxPosition;
+    std::unique_ptr<Boardcore::AnalogEncoder> mainFuelPosition;
+    std::unique_ptr<Boardcore::AnalogEncoder> oxRegPosition;
+    std::unique_ptr<Boardcore::AnalogEncoder> fuelRegPosition;
 
     std::unique_ptr<Boardcore::SensorManager> manager;
 };
