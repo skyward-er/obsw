189d12a706ca2fc262cc4a332122f619de98f37f
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2026-05-18 20:10:37 +0200

    [Main] Added parafoil servos to actuators class
    The new parafoil servos work in whinch mode, which means that they
    interpret the pwm signal as a speed. Because of this they can complete
    multiple rotations, this is done by using a AS5047D magnetic encoder to run
    a closed loop update task to manage their angle at any time.

---
+++ src/common/UnlimitedAngle.h
@@ -0,0 +1,67 @@
+/* Copyright (c) 2018-2026 Skyward Experimental Rocketry
+ * Author: Raul Radu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+#include <units/Angle.h>
+
+namespace Common
+{
+class UnlimitedAngle
+{
+public:
+    UnlimitedAngle() : rounds(0), lastReading(0.0) {}
+
+    void setInitialState(Boardcore::Units::Angle::Radian reading)
+    {
+        lastReading = reading;
+    }
+
+    Boardcore::Units::Angle::Radian getUpdatedAngle(
+        Boardcore::Units::Angle::Radian reading)
+    {
+        auto angleDiff = reading - lastReading;
+
+        if (angleDiff > threshold)
+            rounds--;
+        else if (angleDiff < -threshold)
+            rounds++;
+
+        lastReading = reading;
+        return reading + Boardcore::Units::Angle::Radian(
+                             (Boardcore::Units::Angle::Degree(360.0 * rounds)));
+    }
+
+    Boardcore::Units::Angle::Radian getLastReading()
+    {
+        return lastReading +
+               Boardcore::Units::Angle::Radian(
+                   (Boardcore::Units::Angle::Degree(360.0 * rounds)));
+    }
+
+private:
+    const Boardcore::Units::Angle::Radian threshold =
+        Boardcore::Units::Angle::Radian(
+            (Boardcore::Units::Angle::Degree(320.0)));
+    int rounds;
+    Boardcore::Units::Angle::Radian lastReading;
+};
+}  // namespace Common
