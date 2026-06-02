7205fcac485596c3c241893b555ffa254e2cf049
tom <tommaso.domenicali@mail.polimi.it>
2026-06-02 12:55:07 +0200

    [IMU] Added support for VN100/dual-LSM6 in rotatedIMU

--- src/Main/StateMachines/FlightModeManager/FlightModeManager.cpp
+++ src/Main/StateMachines/FlightModeManager/FlightModeManager.cpp
@@ -765,7 +765,7 @@ State FlightModeManager::state_drogue_descent(const Event& event)
     switch (event)
     {
         case EV_ENTRY:
-        {
+        {            
             LOG_INFO(logger, "Expelled");
 
             updateAndLogStatus(FlightModeManagerState::DROGUE_DESCENT);
@@ -779,6 +779,9 @@ State FlightModeManager::state_drogue_descent(const Event& event)
 
             getModule<Radio>()->disableAscentTelemetry();
             getModule<Radio>()->enableDescentTelemetry();
+
+            // notify the sensors module that we are in descent phase for double LSM6DSRX management
+            getModule<Sensors>()->setAscentPhase(false);
 
             return HANDLED;
         }
