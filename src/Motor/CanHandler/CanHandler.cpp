c7d8e452ddc6e4bcfabda34eeda22b31f87d839a
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2026-08-30 12:52:58 +0200

    [Motor] Update CanHandler to utilize Encoders for valve positions and add calibration event handling

--- src/Motor/CanHandler/CanHandler.cpp
+++ src/Motor/CanHandler/CanHandler.cpp
@@ -193,6 +193,7 @@ bool CanHandler::start()
         [this]()
         {
             Actuators* actuators = getModule<Actuators>();
+            Sensors* sensors     = getModule<Sensors>();
 
             protocol.enqueueData(
                 static_cast<uint8_t>(CanConfig::Priority::HIGH),
@@ -202,7 +203,7 @@ bool CanHandler::start()
                 static_cast<uint8_t>(ServosList::OX_VENTING_VALVE),
                 ServoFeedback{
                     TimestampTimer::getTimestamp(),
-                    actuators->getValvePosition(ServosList::OX_VENTING_VALVE),
+                    sensors->getVentingOxPosition().position,
                     actuators->isValveOpen(ServosList::OX_VENTING_VALVE)});
 
             protocol.enqueueData(
@@ -213,7 +214,7 @@ bool CanHandler::start()
                 static_cast<uint8_t>(ServosList::FUEL_VENTING_VALVE),
                 ServoFeedback{
                     TimestampTimer::getTimestamp(),
-                    actuators->getValvePosition(ServosList::FUEL_VENTING_VALVE),
+                    sensors->getVentingFuelPosition().position,
                     actuators->isValveOpen(ServosList::FUEL_VENTING_VALVE)});
 
             protocol.enqueueData(
@@ -224,7 +225,7 @@ bool CanHandler::start()
                 static_cast<uint8_t>(ServosList::MAIN_OX_VALVE),
                 ServoFeedback{
                     TimestampTimer::getTimestamp(),
-                    actuators->getValvePosition(ServosList::MAIN_OX_VALVE),
+                    sensors->getMainOxPosition().position,
                     actuators->isValveOpen(ServosList::MAIN_OX_VALVE)});
 
             protocol.enqueueData(
@@ -235,7 +236,7 @@ bool CanHandler::start()
                 static_cast<uint8_t>(ServosList::MAIN_FUEL_VALVE),
                 ServoFeedback{
                     TimestampTimer::getTimestamp(),
-                    actuators->getValvePosition(ServosList::MAIN_FUEL_VALVE),
+                    sensors->getMainFuelPosition().position,
                     actuators->isValveOpen(ServosList::MAIN_FUEL_VALVE)});
 
             protocol.enqueueData(
@@ -246,7 +247,7 @@ bool CanHandler::start()
                 static_cast<uint8_t>(ServosList::PRZ_OX_VALVE),
                 ServoFeedback{
                     TimestampTimer::getTimestamp(),
-                    actuators->getValvePosition(ServosList::PRZ_OX_VALVE),
+                    sensors->getPrzOxPosition().position,
                     actuators->isValveOpen(ServosList::PRZ_OX_VALVE)});
 
             protocol.enqueueData(
@@ -419,6 +420,12 @@ void CanHandler::handleEvent(const Canbus::CanMessage& msg)
         {
             EventBroker::getInstance().post(CLOSE_ALL_VALVES,
                                             TOPIC_VALVE_SEQUENCE);
+            break;
+        }
+        case Common::CanConfig::EventId::CALIBRATE:
+        {
+            getModule<Sensors>()->calibrate();
+            getModule<Sensors>()->calibrateEncoders();
             break;
         }
 
--- src/RIGv3/StateMachines/GroundModeManager/GroundModeManager.cpp
+++ src/RIGv3/StateMachines/GroundModeManager/GroundModeManager.cpp
@@ -217,6 +217,8 @@ State GroundModeManager::state_disarmed(const Event& event)
         {
             getModule<Sensors>()->calibrateLoadcells();
             getModule<Sensors>()->calibrateEncoders();
+
+            getModule<CanHandler>()->sendEvent(CanConfig::EventId::CALIBRATE);
             return HANDLED;
         }
 
