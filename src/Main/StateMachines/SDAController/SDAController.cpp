a5ea2c4e886946a8490cc6f0d5af03fe10a6ec73
Pietro Bortolus <pietro.bortolus@skywarder.eu>
2026-09-21 18:00:38 +0200

    [Algorithms][Main] The SDA now runs also in the armed state

--- src/Main/StateMachines/SDAController/SDAController.cpp
+++ src/Main/StateMachines/SDAController/SDAController.cpp
@@ -113,7 +113,8 @@ void SDAController::update()
     // Lock SDA for the whole duration of the update
     Lock<FastMutex> lock{sdaMutex};
 
-    if (curState == SDAControllerState::SHADOW_MODE ||
+    if (curState == SDAControllerState::ARMED ||
+        curState == SDAControllerState::SHADOW_MODE ||
         curState == SDAControllerState::ACTIVE ||
         curState == SDAControllerState::ACTIVE_UNPOWERED)
     {
